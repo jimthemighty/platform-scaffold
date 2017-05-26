8ba1b11e4ce4880cafcf05536be83537fcd1c4e4
Jennifer Bandelin <jennifer@mobify.com>
2017-05-26 13:22:26 -0700

    Fix custom command export

--- web/app/integration-manager/custom/commands.js
+++ web/app/integration-manager/custom/commands.js
@@ -2,10 +2,11 @@
 /* Copyright (c) 2017 Mobify Research & Development Inc. All rights reserved. */
 /* * *  *  * *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * */
 
-let connector = {}
+let customCommands = {}
 
 export const register = (commands) => {
-    connector = commands
+    customCommands = commands
 }
 
 
+export {customCommands}
