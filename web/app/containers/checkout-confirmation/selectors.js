5d364fab181810dd67d5f91f5796a2cbbad5ae77
Cdok <cdokurno@mobify.com>
2017-06-02 11:44:47 -0700

    updated orderNumber selector & confirmation template

--- web/app/containers/checkout-confirmation/selectors.js
+++ web/app/containers/checkout-confirmation/selectors.js
@@ -2,6 +2,7 @@
 /* Copyright (c) 2017 Mobify Research & Development Inc. All rights reserved. */
 /* * *  *  * *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * */
 
+import Immutable from 'immutable'
 import {createSelector} from 'reselect'
 import {createGetSelector} from 'reselect-immutable-helpers'
 import {getUi} from '../../store/selectors'
@@ -9,10 +10,8 @@ import {getUi} from '../../store/selectors'
 export const getCheckoutConfirmation = createSelector(getUi, ({checkoutConfirmation}) => checkoutConfirmation)
 
 export const getIsRegistrationFormHidden = createGetSelector(getCheckoutConfirmation, 'isRegistrationFormHidden')
-export const getConfirmationData = createGetSelector(getCheckoutConfirmation, 'confirmationData')
+export const getConfirmationData = createGetSelector(getCheckoutConfirmation, 'confirmationData', Immutable.Map())
 
-export const getOrderNumber = createSelector(getConfirmationData, (data) => {
-    return data ? data.get('orderNumber') : null
-})
+export const getOrderNumber = createGetSelector(getConfirmationData, 'orderNumber')
 
 export const getOrderUrl = createGetSelector(getCheckoutConfirmation, 'orderUrl')
