d15c6ea2bf8bffec1733095078991f82c882a1b5
jasecode <jzhang@mobify.com>
2017-06-02 15:48:06 -0700

    link circle report_path

--- web/scripts/gather-perf-data.sh
+++ web/scripts/gather-perf-data.sh
@@ -4,7 +4,7 @@ REPO_NAME=${CIRCLE_PROJECT_REPONAME:-Repo Name}
 GIT_HASH=${CIRCLE_SHA1:-Commit}
 BRANCH=${CIRCLE_BRANCH:-Branch}
 TTI=${jsonResults.audits['time-to-interactive'].displayValue}
-REPORT_PATH=https://${BUILD_ID}-${REPO_ID}-gh.circle-artifacts.com/0/${CIRCLE_ARTIFACTS}/audit-local.report.html
+REPORT_PATH=https://${CIRCLE_BUILD_NUM}-${WINDOWID}-gh.circle-artifacts.com/0/${CIRCLE_ARTIFACTS}/audit-local.report.html
 DATE=`date +%Y-%m-%d`
 
 # ID=${CIRCLE_BUILD_NUM:-test}
