9523ab4f3dd2b90b3f9f56eb630cfd3fdd98ed1c
Karl Schmidt <karl@mobify.com>
2017-06-05 14:57:40 -0700

    Remove another change no longer required

--- web/app/containers/app/reducer.js
+++ web/app/containers/app/reducer.js
@@ -13,7 +13,7 @@ import {setPageFetchError, setCheckoutShippingURL, setCartURL, setCurrentURL} fr
 import {CURRENT_URL, FETCHED_PATHS} from './constants'
 
 export const initialState = fromJS({
-    [CURRENT_URL]: '',
+    [CURRENT_URL]: window.location.href,
     fetchError: null,
     [FETCHED_PATHS]: {},
     sprite: '',
