c1d3a18f6fcc359cb699838beefeab8f0038b091
Cassandra de Git <cassandra.m.degit@gmail.com>
2017-06-05 16:04:56 -0700

    Add example data to initHomePage

--- web/app/integration-manager/_stub-connector/home/commands.js
+++ web/app/integration-manager/_stub-connector/home/commands.js
@@ -1,1 +1,23 @@
-export const initHomePage = (url) => (dispatch) => Promise.resolve()
+import {receiveHomeData} from '../../results'
+
+export const initHomePage = (url) => (dispatch) => {
+    const exampleData = {
+        banners: [{
+            src: '//via.placeholder.com/400x200',
+            alt: 'Placeholder Image'
+        }, {
+            src: '//via.placeholder.com/400x200',
+            alt: 'Placeholder Image'
+        }, {
+            src: '//via.placeholder.com/400x200',
+            alt: 'Placeholder Image'
+        }]
+    }
+
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            dispatch(receiveHomeData(exampleData))
+            resolve()
+        })
+    })
+}
