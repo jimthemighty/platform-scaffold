898c6386c6ee451ac6261e21896f964d72561071
Cassandra de Git <cassandra.m.degit@gmail.com>
2017-06-05 16:30:21 -0700

    Add example category data to stub connector

--- web/app/integration-manager/_stub-connector/categories/commands.js
+++ web/app/integration-manager/_stub-connector/categories/commands.js
@@ -1,1 +1,51 @@
-export const initProductListPage = (url, routeName) => (dispatch) => Promise.resolve()
+import {receiveCategoryContents, receiveCategoryInformation} from '../../categories/results'
+import {receiveProductListProductData} from '../../products/results'
+import {urlToPathKey} from 'progressive-web-sdk/dist/utils/utils'
+
+export const initProductListPage = (url, routeName) => (dispatch) => {
+    const thumbnail = {src: '//via.placeholder.com/120x150'}
+    const sharedData = {
+        price: '$10.00',
+        available: true,
+        images: [thumbnail],
+        thumbnail
+    }
+
+    const exampleProductData = {
+        '/product1.html': {
+            id: '1',
+            title: 'Product 1',
+            href: '/product1.html',
+            ...sharedData
+        },
+        '/product2.html': {
+            id: '2',
+            title: 'Product 2',
+            href: '/product2.html',
+            ...sharedData
+        },
+        '/product3.html': {
+            id: '3',
+            title: 'Product 3',
+            href: '/product3.html',
+            ...sharedData
+        }
+    }
+
+    const exampleCategoryData = {
+        itemCount: 3,
+        products: [
+            '/product1.html',
+            '/product2.html',
+            '/product3.html'
+        ]
+    }
+
+    const pathKey = urlToPathKey(url)
+
+    return new Promise((resolve) => {
+        dispatch(receiveProductListProductData(exampleProductData))
+        dispatch(receiveCategoryContents(pathKey, exampleCategoryData))
+        resolve()
+    })
+}
