bc39f5fcb861839dcbbac59ba03193327f3e8ec9
Cole Sanderson <cole@mobify.me>
2017-06-06 15:23:59 -0700

    Tweak app container, still some work to do

--- amp/app/containers/app/container.jsx
+++ amp/app/containers/app/container.jsx
@@ -2,33 +2,78 @@ import React, {PropTypes} from 'react'
 import Header from '../header/container'
 import Footer from '../footer/container'
 import DangerousHTML from '../../components/dangerous-html'
-import Icon from '../../components/icon'
-
+import SkipLinks from '../../components/skip-links'
+import Navigation from '../navigation/container'
 import sprite from '../../static/svg/sprite-dist/sprite.svg'
 
+import {connect} from 'react-redux'
+
 const App = ({
-    children
-}) => (
-    <div
-        id="app"
-        className="t-app"
-    >
-        <DangerousHTML html={sprite}>
-            {(htmlObj) => <div hidden dangerouslySetInnerHTML={htmlObj} />}
-        </DangerousHTML>
+    children,
+    buttontest
+}) => {
 
-        <Icon name="user" title="User" />
+    const skipLinksItems = [
+        // Customize your list of SkipLinks here. These are necessary to
+        // achieve compliance with WCAG 2.0's guideline 2.4.1: "Bypass
+        // Blocks". Compliance is required under some laws, such as the ADA
+        // (Americans with Disabilities Act). For more details, see here:
+        //
+        // @URL: https://www.w3.org/TR/WCAG20-TECHS/G1.html
+        {target: '#app-main', label: 'Skip to content'},
+        {target: '#header-navigation', label: 'Skip to main navigation'},
+        {target: '#app-footer', label: 'Skip to footer'},
+    ]
 
-        <Header />
+    return (
+        <div
+            id="app"
+            className="t-app"
+        >
+            <DangerousHTML html={sprite}>
+                {(htmlObj) => <div hidden dangerouslySetInnerHTML={htmlObj} />}
+            </DangerousHTML>
 
-        {children}
+            <SkipLinks items={skipLinksItems} />
 
-        <Footer />
-    </div>
-)
+            <div id="app-wrap" className="t-app__wrapper u-flexbox u-direction-column">
+                <div id="app-header" className="u-flex-none" role="banner">
+                    <Header />
 
-App.propTypes = {
-    children: PropTypes.node
+                    <Navigation />
+                </div>
+
+                <main id="app-main" className="u-flex" role="main">
+                    {children}
+
+                    <DangerousHTML html={buttontest}>
+                        {(htmlObj) => <div dangerouslySetInnerHTML={htmlObj} />}
+                    </DangerousHTML>
+                </main>
+
+                <div id="app-footer" className="u-flex-none">
+                    <Footer />
+                </div>
+            </div>
+        </div>
+    )
 }
 
-export default App
+
+App.propTypes = {
+    buttontest: PropTypes.string,
+    children: PropTypes.node,
+    /**
+     * The react-router history object
+     */
+    // history: PropTypes.object
+}
+
+
+const mapStateToProps = (state) => ({
+    buttontest: '<button on="tap:menu-sheet.toggle">Button</button>'
+})
+
+export default connect(
+    mapStateToProps
+)(App)
