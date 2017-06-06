bd250cde50c405ae79dcdc53582d51012956d7d5
Sonja <sonjafarrell@gmail.com>
2017-06-06 11:21:29 -0700

    AMP-58: 🐙  merge in amp

--- amp/app/containers/header/container.jsx
+++ amp/app/containers/header/container.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, {PropTypes} from 'react'
 
+import HeaderBar from '../../components/header-bar/'
+import IconLabel from '../../components/icon-label'
+import Link from '../../components/link'
+// import Search from 'progressive-web-sdk/dist/components/search'
+// import HeaderTitle from './partials/header-title'
 
-const Header = () => (
-    <div id="header">
-        <h1>Header</h1>
-    </div>
-)
+const Header = () => {
+    //
+    // const searchIcon = <Icon name="search" title="Submit search" />
+    // const clearIcon = <Icon name="close" title="Clear search field" />
+
+    return (
+        <header className="t-header" id="header">
+            <HeaderBar>
+                <IconLabel iconName="menu" label="menu" size="" />
+                <IconLabel label="search" />
+                <IconLabel label="stores" />
+                <IconLabel label="cart" />
+            </HeaderBar>
+        </header>
+    )
+}
+
 
 export default Header
