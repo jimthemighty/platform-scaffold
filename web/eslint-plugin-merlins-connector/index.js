07ca6a1806fbc0d673e5b7c2900d500f07dedfd1
Cdok <cdokurno@mobify.com>
2017-06-07 15:19:20 -0700

    fixed linting errors in eslint plugin

--- web/eslint-plugin-merlins-connector/index.js
+++ web/eslint-plugin-merlins-connector/index.js
@@ -1,18 +1,18 @@
-module.exports.rules = {
-    "use-merlins": function(context) {
-            return {
-                ImportDeclaration: function (node) {
-                    const specifiers = node.specifiers[0] // always array of length 1
-                    let ruleName
+module.exports.rules = { // eslint-disable-line
+    'use-merlins': (context) => {
+        return {
+            ImportDeclaration: (node) => {
+                const specifiers = node.specifiers[0] // always array of length 1
+                let ruleName
 
-                    if (specifiers.imported) {
-                        ruleName = specifiers.imported.name
-                    }
-
-                    if (ruleName === 'configureConnector' && !/merlins/.test(node.source.value)) {
-                        context.report(node, "Merlin's Connector should be left uncommented");
-                    }
+                if (specifiers.imported) {
+                    ruleName = specifiers.imported.name
                 }
-            };
+
+                if (ruleName === 'configureConnector' && !/merlins/.test(node.source.value)) {
+                    context.report(node, 'Merlin\'s Connector should be left uncommented')
+                }
+            }
         }
-};
+    }
+}
