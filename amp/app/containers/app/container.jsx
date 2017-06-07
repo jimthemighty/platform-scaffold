599d642ad43ea8e553222815497a2f853962d57a
Oliver Brook <olibrook@gmail.com>
2017-06-07 14:33:27 -0700

    JSLint

--- amp/app/containers/app/container.jsx
+++ amp/app/containers/app/container.jsx
@@ -10,7 +10,7 @@ import sprite from '../../static/svg/sprite-dist/sprite.svg'
 const App = ({
     children
 }) => {
-    const button = '<button on="tap:menu-sheet.toggle">Button</button>';
+    const button = '<button on="tap:menu-sheet.toggle">Button</button>'
     return (
         <body
             id="root"
@@ -19,29 +19,29 @@ const App = ({
 
             <Sheet id="menu-sheet" headerContent={<div>Header</div>} footerContent={<div>Footer</div>}>
                 <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Sign in</a></li>
-                    <li><a href="#">Potions</a></li>
-                    <li><a href="#">Spellbooks</a></li>
-                    <li><a href="#">Ingredients</a></li>
-                    <li><a href="#">Supplies</a></li>
-                    <li><a href="#">Charms</a></li>
-                    <li><a href="#">New Arrivals</a></li>
+                    <li><a href="/todo/">Home</a></li>
+                    <li><a href="/todo/">Sign in</a></li>
+                    <li><a href="/todo/">Potions</a></li>
+                    <li><a href="/todo/">Spellbooks</a></li>
+                    <li><a href="/todo/">Ingredients</a></li>
+                    <li><a href="/todo/">Supplies</a></li>
+                    <li><a href="/todo/">Charms</a></li>
+                    <li><a href="/todo/">New Arrivals</a></li>
                     <DangerousHTML html={button}>
-                        {(htmlObj) => <div dangerouslySetInnerHTML={htmlObj}/>}
+                        {(htmlObj) => <div dangerouslySetInnerHTML={htmlObj} />}
                     </DangerousHTML>
                 </ul>
             </Sheet>
 
             <DangerousHTML html={button}>
-                {(htmlObj) => <div dangerouslySetInnerHTML={htmlObj}/>}
+                {(htmlObj) => <div dangerouslySetInnerHTML={htmlObj} />}
             </DangerousHTML>
 
             <DangerousHTML html={sprite}>
-                {(htmlObj) => <div hidden dangerouslySetInnerHTML={htmlObj}/>}
+                {(htmlObj) => <div hidden dangerouslySetInnerHTML={htmlObj} />}
             </DangerousHTML>
 
-            <Icon name="user" title="User"/>
+            <Icon name="user" title="User" />
 
             <Header />
 
