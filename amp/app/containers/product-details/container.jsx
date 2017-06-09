9adca3a9e938a3e3b9f692f5cb07f59cf32cc643
Karl Schmidt <karl@mobify.com>
2017-06-09 16:31:36 -0700

    Address PR feedback, implement basic PDP image rendering

--- amp/app/containers/product-details/container.jsx
+++ amp/app/containers/product-details/container.jsx
@@ -5,12 +5,13 @@ import AmpLightbox from '../../components/amp-lightbox'
 import containerStyles from './container.scss'
 
 import {createPropsSelector} from 'reselect-immutable-helpers'
-import {getProductDescription, getProductTitle} from '../../../../web/app/store/products/selectors'
+import {getProductDescription, getProductTitle, getProductImages} from '../../../../web/app/store/products/selectors'
 
 const containerClass = 't-product-details'
 
 const ProductDetails = ({
     description,
+    images,
     title
 }) => (
     <div className={containerClass}>
@@ -18,28 +19,28 @@ const ProductDetails = ({
         <AmpLightbox id="my-lightbox">
             <AmpImage src="https://media.giphy.com/media/26ufdipQqU2lhNA4g/giphy.gif" width="500" height="500" />
         </AmpLightbox>
-        <AmpImage src="/static/mobify.png" width="252" height="64" layout="fixed" />
 
         <h1>{title}</h1>
         <p>{description}</p>
+
+        <AmpImage src={images[0].src} width="240" height="240" layout="fixed" alt={images[0].alt} />
     </div>
 )
 
 ProductDetails.propTypes = {
-    /**
-     * A description <-- not a good comment!
-     */
-    description: PropTypes.string,
-    /**
-     * A title <-- not a good comment!
-     */
-    title: PropTypes.string
+    description: PropTypes.string.isRequired,
+    images: PropTypes.arrayOf(PropTypes.shape({
+        src: PropTypes.string.isRequired,
+        alt: PropTypes.string
+    })).isRequired,
+    title: PropTypes.string.isRequired
 }
 
 ProductDetails.templateName = 'pdp'
 
 const mapStateToProps = createPropsSelector({
     description: getProductDescription,
+    images: getProductImages,
     title: getProductTitle
 })
 
