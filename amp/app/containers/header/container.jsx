e89e64bf2f0889470393b8fcbf84dfba63ba2632
Sonja <sonjafarrell@gmail.com>
2017-06-12 11:11:03 -0700

    AMP-58: ✨  add in <Button> to header-bar, toggle menu in header bar, tests pass

--- amp/app/containers/header/container.jsx
+++ amp/app/containers/header/container.jsx
@@ -1,33 +1,40 @@
-import React from 'react'
+import React, {PropTypes} from 'react'
 
 import AmpImage from 'mobify-amp-sdk/dist/components/amp-image'
+import Button from '../../components/button'
 import {HeaderBar, HeaderBarTitle} from '../../components/header-bar'
 import IconLabel from '../../components/icon-label'
-import Link from '../../components/link'
 
-const Header = () => {
+const Header = (props) => {
+
+    const {navId} = props
+    const openNav = `tap:${navId}.toggle`
+
     return (
         <header className="t-header" id="header">
             <HeaderBar>
-                <Link href="www.merlinspotions.com" className="t-header__link amp-header-bar__action" role="navigation">
+                <Button className="t-header__link amp-header-bar__action" on={openNav}>
                     <IconLabel iconName="menu" label="Menu" iconSize="medium" />
-                </Link>
-                <Link href="www.merlinspotions.com" className="t-header__link amp-header-bar__action">
+                </Button>
+                <Button className="t-header__link amp-header-bar__action">
                     <IconLabel iconName="search" label="Search" iconSize="medium" />
-                </Link>
+                </Button>
                 <HeaderBarTitle href="/">
                     <AmpImage src="/static/svg/logo.svg" width="67" height="28" layout="fixed" />
                 </HeaderBarTitle>
-                <Link href="www.merlinspotions.com" className="t-header__link amp-header-bar__action">
+                <Button className="t-header__link amp-header-bar__action">
                     <IconLabel iconName="store" label="Stores" iconSize="medium" />
-                </Link>
-                <Link href="www.merlinspotions.com" className="t-header__link amp-header-bar__action">
+                </Button>
+                <Button className="t-header__link amp-header-bar__action">
                     <IconLabel iconName="cart" label="Cart" iconSize="medium" />
-                </Link>
+                </Button>
             </HeaderBar>
         </header>
     )
 }
 
+Header.propTypes = {
+    navId: PropTypes.string
+}
 
 export default Header
