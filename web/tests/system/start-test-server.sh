9c67d01c05c3ff134ee0e36418550b80e6935819
Ellen Wong <ellen@mobify.com>
2017-06-13 10:18:20 -0700

    Simplify start-test-server

--- web/tests/system/start-test-server.sh
+++ web/tests/system/start-test-server.sh
@@ -2,22 +2,19 @@
 set -o pipefail
 set -o nounset
 
-# This script starts the local dev server if the current git branch is not master.
+# Start the local test server if the current git branch is not master.
+# Assumes that tests on master branch should run against production. 
 if git rev-parse ; then
     # Get the current branch on CircleCI or local
     CURRENT_BRANCH=${CIRCLE_BRANCH:-$(git branch | grep "*" | awk '{ print $2 }')}
-else
-    CURRENT_BRANCH=develop
-fi
-
-# Start preview if branch is not master.
-if [ "$CURRENT_BRANCH" != "master" ]; then
-    echo "Running tests against local build."
-    # Kill background processes when this script exits.
-    trap 'kill $(jobs -p)' EXIT
-    echo "Running Test Server."
-    npm run prod:build
-    npm run test:server
+    if [ "$CURRENT_BRANCH" != "master" ]; then
+        echo "Running tests against local build."
+        # Kill background processes when this script exits.
+        trap 'kill $(jobs -p)' EXIT
+        echo "Running Test Server."
+        npm run prod:build
+        npm run test:server
+    fi
 else
     echo "On production branch, test server not needed."
 fi
