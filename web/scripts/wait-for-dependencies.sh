9b258a8a025dd72839c7e39ea115a64fc6542059
jasecode <jzhang@mobify.com>
2017-06-13 13:47:42 -0700

    add signed-cert as seperate script

--- web/scripts/wait-for-dependencies.sh
+++ web/scripts/wait-for-dependencies.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
+
+echo "Waiting for npm prod:build to complete"
+while [ ! -f web/build/loader.js ]; do sleep 2; done
+echo "npm prod:build is complete"
+
 echo "Waiting for dev server to become active"
 while ! nc -z localhost 8443; do sleep 2; done
+echo "Waiting for dev server is now active"
