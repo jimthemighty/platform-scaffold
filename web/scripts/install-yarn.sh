4e06ebba030d6e84410fe5de45d0af909bbe3efb
jasecode <jzhang@mobify.com>
2017-06-13 10:39:45 -0700

    test runtime of yarn install

---
+++ web/scripts/install-yarn.sh
@@ -0,0 +1,6 @@
+#Install Nightly version of Yarn
+#https://yarnpkg.com/en/docs/nightly
+
+sudo apt-key adv --fetch-keys http://dl.yarnpkg.com/debian/pubkey.gpg
+echo "deb http://nightly.yarnpkg.com/debian/ nightly main" | sudo tee /etc/apt/sources.list.d/yarn-nightly.list
+sudo apt-get update && sudo apt-get install yarn
