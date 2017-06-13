09912aa2bec11a1f61cc63cb3d1e5fb71ec4ffa6
jasecode <jzhang@mobify.com>
2017-06-12 17:32:03 -0700

    add comments

--- web/tests/system/run-lighthouse.sh
+++ web/tests/system/run-lighthouse.sh
@@ -2,10 +2,13 @@
 set -o pipefail
 set -o nounset
 
+# Runs automated lighthouse against project url
+# If current branch is master, we test lighthouse on production
+# Otherwise, we run lighthouse against the preview url
+
 if git rev-parse ; then
     # Get the current branch on CircleCI or local
     CURRENT_BRANCH=${CIRCLE_BRANCH:-$(git branch | grep "*" | awk '{ print $2 }')}
-    # If current branch is master, we test lighthouse on production url
     if [ "$CURRENT_BRANCH" == "master" ]; then
         echo "Running lighthouse on Production"
         npm run test:pwa-prod
