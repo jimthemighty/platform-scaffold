a8ad267a4f13030ac49b6d841d8b248b2b7d6966
jasecode <jzhang@mobify.com>
2017-06-13 14:12:25 -0700

    reorganized test steps accross containers

--- web/tests/system/run-all-tests.sh
+++ web/tests/system/run-all-tests.sh
@@ -36,16 +36,16 @@ else
     if [ $CIRCLE_NODE_INDEX -eq 0 ]; then
       echo 'Running Lighthouse Test'
       ./tests/system/run-lighthouse.sh
-    fi
-
-    # The other cirlce_node_index worker will run the rest of the tests
-    if [ $CIRCLE_NODE_INDEX -gt 0 ]; then
+      
       echo 'Running Lint'
       npm run lint
     
       echo 'Running Unit Tests'
       npm test -- --runInBand
-    
+    fi
+
+    # The other cirlce_node_index worker will run the rest of the tests
+    if [ $CIRCLE_NODE_INDEX -gt 0 ]; then    
       echo 'Running End to End Tests'
       #If we have nodes > 2, it will be part of the division to run another test:e2e
       i=0
