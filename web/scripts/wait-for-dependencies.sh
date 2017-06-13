e7568d280340af5aab565ca5a52f84bb583c3f53
jasecode <jzhang@mobify.com>
2017-06-13 14:02:06 -0700

    fix typos in path name.

--- web/scripts/wait-for-dependencies.sh
+++ web/scripts/wait-for-dependencies.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Waiting for npm prod:build to complete"
-while [ ! -f web/build/loader.js ]; do sleep 2; done
+while [ ! -f build/loader.js ]; do sleep 2; done
 echo "npm prod:build is complete"
 
 echo "Waiting for dev server to become active"
