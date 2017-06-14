1e39219613f97458afe11019ab62c1ab8338996e
Cdok <cdokurno@mobify.com>
2017-06-14 15:04:34 -0700

    parser for SFCC search result JSON

--- web/app/integration-manager/_sfcc-connector/parsers.js
+++ web/app/integration-manager/_sfcc-connector/parsers.js
@@ -177,6 +177,12 @@ export const parseSearchSuggestions = ({product_suggestions}) => {
     return suggestions
 }
 
-export const parseSearchResults = ({hits}) => {
-    return hits
-}
+export const parseSearchResults = ({hits}) => hits.map(({product_id, product_name, price, link, orderable}) => {
+    return {
+        id: product_id,
+        title: product_name,
+        price: price.toString(),
+        href: link,
+        available: orderable
+    }
+})
