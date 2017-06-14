66a22e214a228be20af74a5ce8c6452a1d6e4e70
Cole Sanderson <cole@mobify.me>
2017-06-13 17:12:33 -0700

    update Icon Label Button

--- web/app/components/icon-label-button/index.jsx
+++ web/app/components/icon-label-button/index.jsx
@@ -1,7 +1,3 @@
-/* * *  *  * *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * */
-/* Copyright (c) 2017 Mobify Research & Development Inc. All rights reserved. */
-/* * *  *  * *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * */
-
 import React from 'react'
 import classNames from 'classnames'
 import Button from 'progressive-web-sdk/dist/components/button'
@@ -9,20 +5,20 @@ import IconLabel from 'progressive-web-sdk/dist/components/icon-label'
 
 
 const IconLabelButton = (props) => {
-    const {iconName, label, onClick, className} = props
+    const {iconName, label, className} = props
     const classes = classNames('c-icon-label-button', className)
     return (
-        <Button onClick={onClick} className={classes} innerClassName="u-padding-0">
+        <Button className={classes} innerClassName="u-padding-0">
             <IconLabel label={label} iconName={iconName} />
         </Button>
     )
 }
 
+
 IconLabelButton.propTypes = {
     className: React.PropTypes.string,
     iconName: React.PropTypes.string,
-    label: React.PropTypes.string,
-    onClick: React.PropTypes.func,
+    label: React.PropTypes.string
 }
 
 
