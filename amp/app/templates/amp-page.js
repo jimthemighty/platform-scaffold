3b0412fbc12dfd526d7bc3f6edf115d140ff82f8
Cole Sanderson <cole@mobify.me>
2017-06-15 09:24:49 -0700

    Font changes
    - Add Google fonts
    - wf-active class same as PWA
    - remove amp-border-box class, no longer need it

--- amp/app/templates/amp-page.js
+++ amp/app/templates/amp-page.js
@@ -2,7 +2,7 @@ const ampPage = ({title, canonicalURL, body, css, ampScriptIncludes}) => (
     /*eslint-disable */
     `
     <!doctype html>
-    <html amp lang="en" class="amp-border-box">
+    <html amp lang="en" class="wf-active">
         <head>
             <!-- Standard AMP markup -->
             <meta charset="utf-8">
@@ -10,6 +10,7 @@ const ampPage = ({title, canonicalURL, body, css, ampScriptIncludes}) => (
             <title>${title}</title>
             <link rel="canonical" href="${canonicalURL}" />
             <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1, user-scalable=no">
+            <link href="https://fonts.googleapis.com/css?family=Oswald:200,400" rel="stylesheet">
             <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
 
             <!-- AMP Component JS includes go here -->
