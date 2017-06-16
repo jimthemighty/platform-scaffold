35c33f4ea21c3d4eae6b5e9578301091ad4e38f8
jasecode <jzhang@mobify.com>
2017-06-16 16:07:47 -0700

    repositioned chrome and accept cert in setup-env-ci.sh

--- web/scripts/setup-env-ci.sh
+++ web/scripts/setup-env-ci.sh
@@ -4,9 +4,9 @@
 mkdir logs
 printf "\nInstall Chrome: "
 nohup bash -c "./scripts/install-google-chrome-stable.sh &> logs/installChrome.log &"
-printf "\nAccept Cert: "
-nohup bash -c "./scripts/add-self-signed-cert-on-ci.sh &> logs/addSignedCert.log &"
 printf "\nNPM Install: "
 npm install
+printf "\nAccept Cert: "
+nohup bash -c "./scripts/add-self-signed-cert-on-ci.sh &> logs/addSignedCert.log &"
 printf "\nStart Test Server: "
 nohup bash -c "./tests/system/start-test-server.sh &> logs/startTestServer.log &"
