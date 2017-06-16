4eb2200403aa4103aa25a843c386b8b02dc2e372
Jeff Kamo <kamojeffrey@gmail.com>
2017-06-16 10:44:14 -0700

    Add placeholder children to navigation state

--- web/app/containers/home/reducer.js
+++ web/app/containers/home/reducer.js
@@ -8,10 +8,7 @@ import {mergePayload} from '../../utils/reducer-utils'
 
 import {receiveHomeData} from '../../integration-manager/results'
 
-const CATEGORY_PLACEHOLDER_COUNT = 6
-
 const initialState = fromJS({
-    categories: new Array(CATEGORY_PLACEHOLDER_COUNT).fill({}),
     banners: []
 })
 
