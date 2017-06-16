419398f9d67e0ebfd4e9b19b20e3dc3df85da371
Cole Sanderson <cole@mobify.me>
2017-06-16 16:36:24 -0700

    move around content in partials and work with sheet

--- amp/app/containers/product-list/container.jsx
+++ amp/app/containers/product-list/container.jsx
@@ -1,10 +1,6 @@
 import React, {PropTypes} from 'react'
 import {connect} from 'react-redux'
 import containerStyles from './container.scss'
-
-import List from '../../components/list'
-import ProductTile from '../../components/product-tile'
-
 import {createPropsSelector} from 'reselect-immutable-helpers'
 import {getFilteredAndSortedListProducts} from '../../../../web/app/containers/product-list/selectors'
 import {getCategoryItemCount} from '../../../../web/app/store/categories/selectors'
@@ -13,42 +9,25 @@ import {CURRENT_URL} from '../../../../web/app/containers/app/constants'
 
 // Partials
 import ProductListHeader from './partials/product-list-header'
-// import ProductListContents from './partials/product-list-contents'
+import ProductListContents from './partials/product-list-contents'
+import ProductListFilterModal from './partials/product-list-filter-modal'
 
-const ProductList = ({
-    numItems,
-    products
-}) => (
+const ProductList = () => {
+    const filterModalId = 'filter-modal'
 
-    <div className="t-product-list">
-        <ProductListHeader />
+    return (
+        <div className="t-product-list">
+            <ProductListHeader />
 
-        <div className="t-product-list__container u-padding-end u-padding-bottom-lg u-padding-start">
-            <p className="t-product-list__num-results">{numItems} Results</p>
-            <List>
-                {
-                    products.map((prod) =>
-                        <ProductTile
-                            href="/"
-                            key={prod.id}
-                            price={prod.price}
-                            thumbnail={{
-                                alt: prod.title,
-                                src: prod.thumbnail.src
-                            }}
-                            title={prod.title}
-                        />
-                    )
-                }
-            </List>
+            <ProductListContents sheetId={filterModalId} /> {/* TODO FIX HOW TO WORK WITH route name eg. <ProductListContents routeName={routeName} /> */}
+            <ProductListFilterModal sheetId={filterModalId} />
         </div>
-    </div>
-)
-
+    )
+}
 
 ProductList.propTypes = {
-    products: PropTypes.array.isRequired,
-    numItems: PropTypes.number
+    // Route object added by react router
+    route: PropTypes.object
 }
 
 ProductList.resolves = [({dispatch, getState}) => {
--- amp/app/containers/product-list/partials/product-list-contents.jsx
+++ amp/app/containers/product-list/partials/product-list-contents.jsx
@@ -1,33 +1,41 @@
-/* * *  *  * *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * */
-/* Copyright (c) 2017 Mobify Research & Development Inc. All rights reserved. */
-/* * *  *  * *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * */
-
 import React, {PropTypes} from 'react'
 import {connect} from 'react-redux'
 import {createPropsSelector} from 'reselect-immutable-helpers'
-import {getCategoryItemCount} from '../../../store/categories/selectors'
-import * as selectors from '../selectors'
+import * as selectors from '../../../../../web/app/containers/product-list/selectors'
 import {staticURL} from '../../../utils'
-import {changeSort} from '../actions'
+import {getCategoryItemCount} from '../../../../../web/app/store/categories/selectors'
 
 import AmpImage from 'mobify-amp-sdk/dist/components/amp-image'
 import Button from '../../../components/button'
 import Field from '../../../components/field'
-
-// import List from 'progressive-web-sdk/dist/components/list'
+import List from '../../../components/list'
+import ProductTile from '../../../components/product-tile'
 
 const noResultsText = 'We can\'t find products matching the selection'
 const emptySearchText = 'Your search returned no results. Please check your spelling and try searching again.'
 
-// const ResultList = ({products}) => (
-//     <List className="c--borderless">
-//         {products.map((product, idx) => <ProductTile key={product ? product.id : idx} {...product} />)}
-//     </List>
-// )
-//
-// ResultList.propTypes = {
-//     products: PropTypes.array
-// }
+const ResultList = ({products}) => (
+    <List className="amp--borderless">
+        {
+            products.map((prod) =>
+                <ProductTile
+                    href="/"
+                    key={prod.id}
+                    price={prod.price}
+                    thumbnail={{
+                        alt: prod.title,
+                        src: prod.thumbnail.src
+                    }}
+                    title={prod.title}
+                />
+            )
+        }
+    </List>
+)
+
+ResultList.propTypes = {
+    products: PropTypes.array
+}
 
 const NoResultsList = ({routeName}) => (
     <div className="u-flexbox u-direction-column u-align-center">
@@ -50,65 +58,65 @@ NoResultsList.propTypes = {
     routeName: PropTypes.string
 }
 
-const ProductListContents = ({
-    activeFilters,
-    products,
-    sortChange,
-    routeName
-}) => (
-    <div>
 
-        <div className="t-product-list__container u-padding-end u-padding-bottom-lg u-padding-start">
-            <div className="t-product-list__num-results u-padding-md u-padding-start-sm u-padding-end-sm">
-                <div>
-                    {products.length > 0 &&
-                        <div className="u-flexbox">
-                            <div className="t-product-list__filter u-flex u-margin-end-md">
-                                <Field
-                                    idForLabel="filterButton"
-                                    label={`${products.length} Items`}
-                                >
-                                    <Button
-                                        className="c--tertiary u-width-full u-text-uppercase"
-                                        disabled={routeName === 'searchResultPage' || activeFilters.length > 0}
-                                        id="filterButton"
-                                    >
-                                        Filter
-                                    </Button>
-                                </Field>
-                            </div>
+const ProductListContents = (props) => {
+    const {sheetId, products, routeName, activeFilters} = props
 
-                            <div className="t-product-list__sort u-flex">
-                                <Field
-                                    className="pw--has-select"
-                                    idForLabel="sort"
-                                    label="Sort by"
-                                >
-                                    <select
-                                        className="u-color-neutral-60"
-                                        onChange={(e) => { sortChange(e.target.value) }}
-                                        onBlur={(e) => { sortChange(e.target.value) }}
+    const toggleFilterSheet = `tap:${sheetId}.toggle`
+
+    return (
+        <div>
+            <div className="t-product-list__container u-padding-end u-padding-bottom-lg u-padding-start">
+                <div className="t-product-list__num-results u-padding-md u-padding-start-sm u-padding-end-sm">
+                    <div>
+                        {products.length > 0 &&
+                            <div className="u-flexbox">
+                                <div className="t-product-list__filter u-flex u-margin-end-md">
+                                    <Field
+                                        idForLabel="filterButton"
+                                        label={`${products.length} Items`}
                                     >
-                                        {/* This list of options corresponds to the functions in app/utils/sort-utils.js */}
-                                        <option value="position">Position</option>
-                                        <option value="name">Name</option>
-                                        <option value="price">Price</option>
-                                    </select>
-                                </Field>
+                                        <Button
+                                            className="amp--tertiary u-width-full u-text-uppercase"
+                                            disabled={routeName === 'searchResultPage' || activeFilters.length > 0}
+                                            id="filterButton"
+                                            on={toggleFilterSheet}
+                                        >
+                                            Filter
+                                        </Button>
+                                    </Field>
+                                </div>
+
+                                <div className="t-product-list__sort u-flex">
+                                    <Field
+                                        className="amp--has-select"
+                                        idForLabel="sort"
+                                        label="Sort by"
+                                    >
+                                        <select
+                                            className="u-color-neutral-60"
+                                        >
+                                            {/* This list of options corresponds to the functions in app/utils/sort-utils.js */}
+                                            <option value="position">Position</option>
+                                            <option value="name">Name</option>
+                                            <option value="price">Price</option>
+                                        </select>
+                                    </Field>
+                                </div>
                             </div>
-                        </div>
-                    }
+                        }
+                    </div>
                 </div>
-            </div>
 
-            {(products.length > 0) ?
-                {/* <ResultList products={products} /> */}
-            :
-                <NoResultsList routeName={routeName} />
-            }
+                {(products.length > 0) ?
+                    <ResultList products={products} />
+                :
+                    <NoResultsList routeName={routeName} />
+                }
+            </div>
         </div>
-    </div>
-)
+    )
+}
 
 
 ProductListContents.propTypes = {
@@ -116,21 +124,15 @@ ProductListContents.propTypes = {
     activeFilters: PropTypes.array,
     numItems: PropTypes.number,
     routeName: PropTypes.string,
-    sortChange: PropTypes.func
+    sheetId: PropTypes.string
 }
 
+
 const mapStateToProps = createPropsSelector({
-    numItems: getCategoryItemCount,
     activeFilters: selectors.getActiveFilters,
+    numItems: getCategoryItemCount,
     products: selectors.getFilteredAndSortedListProducts
 })
 
-const mapDispatchToProps = {
-    sortChange: changeSort,
-}
 
-
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(ProductListContents)
+export default connect(mapStateToProps)(ProductListContents)
