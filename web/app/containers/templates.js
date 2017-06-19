96e685ba54cc66ef5f6f8d960bf69f85d148bb7a
Tony Dinh <im.tonydinh@gmail.com>
2017-06-19 15:22:08 -0700

    web-1395: cleanup

--- web/app/containers/templates.js
+++ web/app/containers/templates.js
@@ -15,13 +15,13 @@ const isRunningInIOS = /ip(hone|ad)/i.test(navigator.userAgent)
 const loadableList = []
 const prefetchFilenames = []
 
-const PWALoadable = (loader, chunkFileName) => {
+const PWALoadable = (loader, chunkName) => {
     const loadable = Loadable({
         loader,
         LoadingComponent: ContainerPlaceholder
     })
     loadableList.push(loadable)
-    prefetchFilenames.push(chunkFileName)
+    prefetchFilenames.push(`${chunkName}.js`)
     return loadable
 }
 
@@ -33,7 +33,7 @@ const registerPreloadCallbacks = () => {
 
 export const prefetchTemplateChunks = () => {
     // iOS browsers do not support prefetch link tags so
-    // register them to be preloaded when browser is idle
+    // register the loadables to be preloaded when browser is idle
     if (isRunningInIOS) {
         registerPreloadCallbacks()
     } else {
@@ -45,10 +45,10 @@ export const prefetchTemplateChunks = () => {
 
 // These are on the old model and need to be wrapped here
 // rather than in container.js to avoid circular imports
-export const Cart = PWALoadable(() => import('./cart/container' /* webpackChunkName: "cart" */), 'cart.js')
-export const CheckoutConfirmation = PWALoadable(() => import('./checkout-confirmation/container' /* webpackChunkName: "checkout-confirmation" */), 'checkout-confirmation.js')
-export const CheckoutPayment = PWALoadable(() => import('./checkout-payment/container' /* webpackChunkName: "checkout-payment" */), 'checkout-payment.js')
-export const CheckoutShipping = PWALoadable(() => import('./checkout-shipping/container' /* webpackChunkName: "checkout-shipping" */), 'checkout-shipping.js')
-export const Login = PWALoadable(() => import('./login/container' /* webpackChunkName: "login" */), 'login.js')
-export const ProductDetails = PWALoadable(() => import('./product-details/container' /* webpackChunkName: "product-details" */), 'product-details.js')
-export const ProductList = PWALoadable(() => import('./product-list/container' /* webpackChunkName: "product-list" */), 'product-list.js')
+export const Cart = PWALoadable(() => import('./cart/container' /* webpackChunkName: "cart" */), 'cart')
+export const CheckoutConfirmation = PWALoadable(() => import('./checkout-confirmation/container' /* webpackChunkName: "checkout-confirmation" */), 'checkout-confirmation')
+export const CheckoutPayment = PWALoadable(() => import('./checkout-payment/container' /* webpackChunkName: "checkout-payment" */), 'checkout-payment')
+export const CheckoutShipping = PWALoadable(() => import('./checkout-shipping/container' /* webpackChunkName: "checkout-shipping" */), 'checkout-shipping')
+export const Login = PWALoadable(() => import('./login/container' /* webpackChunkName: "login" */), 'login')
+export const ProductDetails = PWALoadable(() => import('./product-details/container' /* webpackChunkName: "product-details" */), 'product-details')
+export const ProductList = PWALoadable(() => import('./product-list/container' /* webpackChunkName: "product-list" */), 'product-list')
