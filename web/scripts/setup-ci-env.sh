bc7a0cbb2fbd388b23fa47aa3aeccac9644b1d18
jasecode <jzhang@mobify.com>
2017-06-19 15:36:17 -0700

    delete old script. fixed new one

--- web/scripts/setup-ci-env.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-#Install all depenecies
-mkdir logs
-./scripts/install-google-chrome-stable.sh > logs/installChrome.log 2>&1 &
-./scripts/add-self-signed-cert.sh > logs/addSignedCert.log 2>&1 &
-npm install
-./tests/system/test-scripts/start-test-server.sh > logs/startTestServer.log 2>&1 &
