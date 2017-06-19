7939d5759504d7625642a9ed7c1dd16f7ccaeb7c
Tony Dinh <im.tonydinh@gmail.com>
2017-06-19 15:11:37 -0700

    web-1395: reload template files for ios

--- web/app/containers/templates.js
+++ web/app/containers/templates.js
@@ -9,39 +9,46 @@ import Loadable from 'react-loadable'
 import {getBuildOrigin} from 'progressive-web-sdk/dist/asset-utils'
 import ContainerPlaceholder from '../components/container-placeholder'
 import {prefetchLink} from '../utils/loader-utils'
+import {requestIdleCallback} from '../utils/utils'
 
-const PWALoadable = (loader) => {
+const isRunningInIOS = /ip(hone|ad)/i.test(navigator.userAgent)
+const loadableList = []
+const prefetchFilenames = []
+
+const PWALoadable = (loader, chunkFileName) => {
     const loadable = Loadable({
         loader,
         LoadingComponent: ContainerPlaceholder
     })
+    loadableList.push(loadable)
+    prefetchFilenames.push(chunkFileName)
     return loadable
 }
 
-// A list of filenames that we want to be prefetched by the browser
-// these filenames come from the chunk names defined below.
-const prefetchFilenames = [
-    'cart.js',
-    'checkout-confirmation.js',
-    'checkout-payment.js',
-    'checkout-shipping.js',
-    'login.js',
-    'product-details.js',
-    'product-list.js'
-]
+const registerPreloadCallbacks = () => {
+    loadableList.forEach((loadable) => {
+        requestIdleCallback(() => loadable.preload())
+    })
+}
 
 export const prefetchTemplateChunks = () => {
-    prefetchFilenames
-        .map((filename) => `${getBuildOrigin()}${filename}`)
-        .forEach((link) => prefetchLink({href: link}))
+    // iOS browsers do not support prefetch link tags so
+    // register them to be preloaded when browser is idle
+    if (isRunningInIOS) {
+        registerPreloadCallbacks()
+    } else {
+        prefetchFilenames
+            .map((filename) => `${getBuildOrigin()}${filename}`)
+            .forEach((link) => prefetchLink({href: link}))
+    }
 }
 
 // These are on the old model and need to be wrapped here
 // rather than in container.js to avoid circular imports
-export const Cart = PWALoadable(() => import('./cart/container' /* webpackChunkName: "cart" */))
-export const CheckoutConfirmation = PWALoadable(() => import('./checkout-confirmation/container' /* webpackChunkName: "checkout-confirmation" */))
-export const CheckoutPayment = PWALoadable(() => import('./checkout-payment/container' /* webpackChunkName: "checkout-payment" */))
-export const CheckoutShipping = PWALoadable(() => import('./checkout-shipping/container' /* webpackChunkName: "checkout-shipping" */))
-export const Login = PWALoadable(() => import('./login/container' /* webpackChunkName: "login" */))
-export const ProductDetails = PWALoadable(() => import('./product-details/container' /* webpackChunkName: "product-details" */))
-export const ProductList = PWALoadable(() => import('./product-list/container' /* webpackChunkName: "product-list" */))
+export const Cart = PWALoadable(() => import('./cart/container' /* webpackChunkName: "cart" */), 'cart.js')
+export const CheckoutConfirmation = PWALoadable(() => import('./checkout-confirmation/container' /* webpackChunkName: "checkout-confirmation" */), 'checkout-confirmation.js')
+export const CheckoutPayment = PWALoadable(() => import('./checkout-payment/container' /* webpackChunkName: "checkout-payment" */), 'checkout-payment.js')
+export const CheckoutShipping = PWALoadable(() => import('./checkout-shipping/container' /* webpackChunkName: "checkout-shipping" */), 'checkout-shipping.js')
+export const Login = PWALoadable(() => import('./login/container' /* webpackChunkName: "login" */), 'login.js')
+export const ProductDetails = PWALoadable(() => import('./product-details/container' /* webpackChunkName: "product-details" */), 'product-details.js')
+export const ProductList = PWALoadable(() => import('./product-list/container' /* webpackChunkName: "product-list" */), 'product-list.js')
