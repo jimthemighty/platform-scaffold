fe3948e80662c51803e400a5079baab6c7e9253d
jasecode <jzhang@mobify.com>
2017-06-19 15:12:44 -0700

    update path for start-test-server

--- web/scripts/setup-ci-env.sh
+++ web/scripts/setup-ci-env.sh
@@ -4,4 +4,4 @@ mkdir logs
 ./scripts/install-google-chrome-stable.sh > logs/installChrome.log 2>&1 &
 ./scripts/add-self-signed-cert.sh > logs/addSignedCert.log 2>&1 &
 npm install
-./tests/system/start-test-server.sh > logs/startTestServer.log 2>&1 &
+./tests/system/test-scripts/start-test-server.sh > logs/startTestServer.log 2>&1 &
