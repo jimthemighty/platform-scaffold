16e2df988de43c1b83bc44e9625fe6ecabd821ff
Oliver Brook <olibrook@gmail.com>
2017-06-20 11:23:24 -0700

    Whoops - include custom themes that have been created for Merlins in the style config.

--- amp/app/config/styles.js
+++ amp/app/config/styles.js
@@ -5,30 +5,31 @@ import AmpLightboxTheme from '../components/amp-lightbox/_theme.scss'
 
 import Breadcrumbs from '../components/breadcrumbs/index'
 import BreadcrumbsBase from '../components/breadcrumbs/_base.scss'
-import BreadcrumbsTheme from '../components/breadcrumbs/_theme.scss'
+import BreadcrumbsTheme from '../styles/themes/amp-components/_breadcrumbs.scss'
 
 import Button from '../components/button/index'
 import ButtonBase from '../components/button/_base.scss'
+import ButtonTheme from '../styles/themes/amp-components/_button.scss'
 
 import Field from '../components/field/index'
 import FieldBase from '../components/field/_base.scss'
-import FieldTheme from '../components/field/_theme.scss'
+import FieldTheme from '../styles/themes/amp-components/_field.scss'
 
 import FieldRow from '../components/field-row/index'
 import FieldRowBase from '../components/field-row/_base.scss'
-import FieldRowTheme from '../components/field-row/_theme.scss'
+import FieldRowTheme from '../styles/themes/amp-components/_field-row.scss'
 
 import {HeaderBar} from '../components/header-bar/index'
 import HeaderBarBase from '../components/header-bar/_base.scss'
-import HeaderBarTheme from '../components/header-bar/_theme.scss'
+import HeaderBarTheme from '../styles/themes/amp-components/_header-bar.scss'
 
 import Icon from '../components/icon/index'
 import IconBase from '../components/icon/_base.scss'
-import IconTheme from '../components/icon/_theme.scss'
+import IconTheme from '../styles/themes/amp-components/_icon.scss'
 
 import IconLabel from '../components/icon-label/index'
 import IconLabelBase from '../components/icon-label/_base.scss'
-import IconLabelTheme from '../components/icon-label/_theme.scss'
+import IconLabelTheme from '../styles/themes/amp-components/_icon-label.scss'
 
 import IconLabelButton from '../components/icon-label-button/index'
 import IconLabelButtonBase from '../components/icon-label-button/_base.scss'
@@ -37,26 +38,31 @@ import Link from '../components/link/index'
 import LinkBase from '../components/link/_base.scss'
 import LinkTheme from '../components/link/_theme.scss'
 
+import List from '../components/list/index'
+import ListTheme from '../styles/themes/amp-components/_list.scss'
+
 import ListTile from '../components/list-tile/index'
 import ListTileBase from '../components/list-tile/_base.scss'
-import ListTileTheme from '../components/list-tile/_theme.scss'
+import ListTileTheme from '../styles/themes/amp-components/_list-tile.scss'
 
 import Nav from '../components/nav/index'
 import NavBase from '../components/nav/_base.scss'
+import NavTheme from '../styles/themes/amp-components/_nav.scss'
 
 import NavItem from '../components/nav-item/index'
 import NavItemBase from '../components/nav-item/_base.scss'
-import NavItemTheme from '../components/nav-item/_theme.scss'
+import NavItemTheme from '../styles/themes/amp-components/_nav-item.scss'
 
 import NavMenu from '../components/nav-menu/index'
 import NavMenuBase from '../components/nav-menu/_base.scss'
-import NavMenuTheme from '../components/nav-menu/_theme.scss'
+import NavMenuTheme from '../styles/themes/amp-components/_nav-menu.scss'
 
 import ProductItem from '../components/product-item/index'
 import ProductItemBase from '../components/product-item/_base.scss'
 
 import ProductTile from '../components/product-tile/index'
 import ProductTileBase from '../components/product-tile/_base.scss'
+import ProductTileTheme from '../styles/themes/amp-components/_product-tile.scss'
 
 import Sheet from '../components/sheet/index'
 import SheetBase from '../components/sheet/_base.scss'
@@ -89,7 +95,7 @@ const styles = new Map()
 
 styles.set(AmpLightbox, [AmpLightboxBase, AmpLightboxTheme])
 styles.set(Breadcrumbs, [BreadcrumbsBase, BreadcrumbsTheme])
-styles.set(Button, [ButtonBase])
+styles.set(Button, [ButtonBase, ButtonTheme])
 styles.set(Field, [FieldBase, FieldTheme])
 styles.set(FieldRow, [FieldRowBase, FieldRowTheme])
 styles.set(HeaderBar, [HeaderBarBase, HeaderBarTheme])
@@ -97,12 +103,13 @@ styles.set(Icon, [IconBase, IconTheme])
 styles.set(IconLabel, [IconLabelBase, IconLabelTheme])
 styles.set(IconLabelButton, [IconLabelButtonBase])
 styles.set(Link, [LinkBase, LinkTheme])
+styles.set(List, [ListTheme])
 styles.set(ListTile, [ListTileBase, ListTileTheme])
-styles.set(Nav, [NavBase])
+styles.set(Nav, [NavBase, NavTheme])
 styles.set(NavItem, [NavItemBase, NavItemTheme])
 styles.set(NavMenu, [NavMenuBase, NavMenuTheme])
 styles.set(ProductItem, [ProductItemBase])
-styles.set(ProductTile, [ProductTileBase])
+styles.set(ProductTile, [ProductTileBase, ProductTileTheme])
 styles.set(Sheet, [SheetBase, SheetTheme])
 styles.set(SkipLinks, [SkipLinksBase, SkipLinksTheme])
 
