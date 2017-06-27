e2dff2d1866eb1e973490466acaf1c6976244b33
Cole Sanderson <cole@mobify.me>
2017-06-27 14:25:38 -0700

    move carousel in partial

--- amp/app/containers/product-details/container.jsx
+++ amp/app/containers/product-details/container.jsx
@@ -1,4 +1,4 @@
-import React, {PropTypes} from 'react'
+import React from 'react'
 import {connect} from 'react-redux'
 import {createPropsSelector} from 'reselect-immutable-helpers'
 import {getProductImages} from '../../../../web/app/store/products/selectors'
@@ -7,32 +7,12 @@ import {CURRENT_URL} from '../../../../web/app/containers/app/constants'
 // Selectors
 import {initProductDetailsPage} from '../../../../web/app/integration-manager/products/commands'
 
-// Components
-import Carousel from '../../components/carousel'
-import AmpImage from 'mobify-amp-sdk/dist/components/amp-image'
+// Partials
+import ProductDetailsCarousel from './partials/product-details-carousel'
 
-
-const ProductDetails = ({images}) => (
+const ProductDetails = () => (
     <div className="t-product-details">
-        <Carousel
-            id="product-details-carousel"
-            previousIcon="chevron-left"
-            nextIcon="chevron-right"
-            className="amp--frame amp--side-controls t-product-details__carousel u-padding-md u-bg-color-neutral-10"
-            showControls={images.length > 1}
-            height="330"
-            width="330"
-        >
-            {images.map(({src, alt = ''}) => (
-                <AmpImage
-                    className="u-display-block"
-                    height="330"
-                    width="330"
-                    alt={alt}
-                    src={src}
-                    key={src} />
-            ))}
-        </Carousel>
+        <ProductDetailsCarousel />
     </div>
 )
 
@@ -41,13 +21,6 @@ ProductDetails.resolves = [({dispatch, getState}) => {
 }]
 
 ProductDetails.templateName = 'pdp'
-
-ProductDetails.propTypes = {
-    images: PropTypes.arrayOf(PropTypes.shape({
-        src: PropTypes.string.isRequired,
-        alt: PropTypes.string
-    })).isRequired
-}
 
 const mapStateToProps = createPropsSelector({
     images: getProductImages
