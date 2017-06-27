07d2d5b032153e0d51ace0e4f738ae8b40dec4d0
gsaray <gsaray@mobify.com>
2017-06-27 15:33:56 -0700

    add skeleton

--- web/app/containers/account/container.jsx
+++ web/app/containers/account/container.jsx
@@ -8,6 +8,7 @@ import {createPropsSelector} from 'reselect-immutable-helpers'
 import template from '../../template'
 
 import {getTitle, getText} from './selectors'
+import SkeletonText from 'progressive-web-sdk/dist/components/skeleton-text'
 // import * as accountActions from './actions'
 
 const containerClass = 't-account'
@@ -15,7 +16,11 @@ const titleClass = `${containerClass}__title`
 
 const Account = ({title, text}) => (
     <div className={containerClass}>
-        <h1 className={titleClass}>{title}</h1>
+        { title ?
+            <h1 className={titleClass}>{title}</h1>
+        :
+            <SkeletonText lines={1} type="h1" width="100px" />
+        }
         {text.map((paragraph, idx) => <p key={idx}>{paragraph}</p>)}
     </div>
 )
