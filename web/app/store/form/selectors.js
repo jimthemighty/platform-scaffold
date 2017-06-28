824c7acb67b89644e6b0a8c992a36cd284524dd0
Jennifer Bandelin <jennifer@mobify.com>
2017-06-28 16:00:36 -0700

    Remove getFormAddressValues selector

--- web/app/store/form/selectors.js
+++ web/app/store/form/selectors.js
@@ -12,17 +12,17 @@ import {
     SHIPPING_FORM_NAME
 } from './constants'
 
-import {getFormValues, getFormAddressValues} from 'progressive-web-sdk/dist/store/form/selectors'
+import {getFormValues} from 'progressive-web-sdk/dist/store/form/selectors'
 
 export const getForm = ({form}) => form
 
-export const getEstimateShippingAddress = getFormAddressValues(ESTIMATE_FORM_NAME)
+export const getEstimateShippingAddress = getFormValues(ESTIMATE_FORM_NAME)
 
 export const getShippingFormValues = getFormValues(SHIPPING_FORM_NAME)
 
 export const getShippingSavedAddressID = createSelector(getShippingFormValues, ({savedAddress}) => savedAddress)
 
-export const getShippingEstimateAddress = getFormAddressValues(SHIPPING_FORM_NAME)
+export const getShippingEstimateAddress = getFormValues(SHIPPING_FORM_NAME)
 
 export const getPaymentBillingFormValues = getFormValues(PAYMENT_FORM_NAME)
 export const getPaymentBillingCCNumber = createSelector(getPaymentBillingFormValues, (form) => {
