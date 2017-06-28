c54183d9e6efac43f4eb4cce6d7a73c3903721ed
gsaray <gsaray@mobify.com>
2017-06-28 15:03:52 -0700

    rename account to account-dashboard

--- web/app/containers/account-dashboard/container.jsx
+++ web/app/containers/account-dashboard/container.jsx
@@ -9,25 +9,34 @@ import template from '../../template'
 
 import {getTitle, getLink} from './selectors'
 import SkeletonText from 'progressive-web-sdk/dist/components/skeleton-text'
-// import * as accountActions from './actions'
+import ListTile from 'progressive-web-sdk/dist/components/list-tile'
+import Icon from 'progressive-web-sdk/dist/components/icon'
 
-const containerClass = 't-account'
-const titleClass = `${containerClass}__title`
+
+const containerClass = 't-account-dashboard'
+const titleClass = 't-account-dashboard__title u-padding-md'
 
 const Account = ({title, links}) => (
     <div className={containerClass}>
         { title ?
             <h1 className={titleClass}>{title}</h1>
         :
-            <SkeletonText lines={1} type="h1" width="100px" />
+            <SkeletonText className="u-padding-md" lines={1} type="h1" width="100px" />
         }
-        <ul>
+        <div className="u-bg-color-neutral-00 u-border-bottom u-border-top">
             {links.map((link, idx) => {
                 return (
-                    <li key={idx}>{link.text}</li>
+                    <ListTile
+                        key={idx}
+                        href={link.href}
+                        endAction={<Icon name="chevron-right" />}
+                        className="t-account-dashboard__link"
+                    >
+                        {link.text}
+                    </ListTile>
                 )
             })}
-        </ul>
+        </div>
     </div>
 )
 
@@ -41,11 +50,4 @@ const mapStateToProps = createPropsSelector({
     links: getLink
 })
 
-const mapDispatchToProps = {
-    // setTitle: accountActions.setTitle
-}
-
-export default template(connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(Account))
+export default template(connect(mapStateToProps)(Account))
