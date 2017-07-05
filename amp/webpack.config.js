557f6b8c88cb35d9750291a848478e6d47c5dee8
Cole Sanderson <cole@mobify.me>
2017-07-05 11:07:41 -0700

    cleanup, easier to read

--- amp/webpack.config.js
+++ amp/webpack.config.js
@@ -30,8 +30,13 @@ module.exports = {
             {
                 test: /\.scss$/,
                 use: [
-                    {loader: 'css-loader?-autoprefixer&-url', options: {minimize : true}},
-                    {loader: 'postcss-loader'},
+                    {
+                        loader: 'css-loader?-autoprefixer&-url',
+                        options: {minimize : true}
+                    },
+                    {
+                        loader: 'postcss-loader'
+                    },
                     {
                         loader: 'sass-loader',
                         options: {includePaths: [path.join(__dirname, 'app', 'styles')]}
