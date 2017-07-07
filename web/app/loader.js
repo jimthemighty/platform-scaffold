60edaf1ddd2a73f7e750d18de91234cfc53ad00d
Jeff Kamo <kamojeffrey@gmail.com>
2017-07-06 18:07:33 -0700

    Remove inaccessible meta properties

--- web/app/loader.js
+++ web/app/loader.js
@@ -195,7 +195,7 @@ const attemptToInitializeApp = () => {
         /* eslint-disable max-len */
         loadAsset('meta', {
             name: 'viewport',
-            content: 'width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no'
+            content: 'width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=5.0'
         })
         /* eslint-enable max-len */
 
