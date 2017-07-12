c71a63884f5bc18afae5de12d42b02b729a399a8
Kevin He <khe@mobify.me>
2017-07-11 17:02:32 -0700

    Fix search selector

--- web/app/store/categories/selectors.js
+++ web/app/store/categories/selectors.js
@@ -28,7 +28,7 @@ export const getCategoryProductIds = createGetSelector(getSelectedCategory, 'pro
 export const getCategoryItemCount = createGetSelector(getSelectedCategory, 'itemCount')
 export const getCategoryTitle = createGetSelector(getCategory, 'title')
 export const getCategoryParentID = createGetSelector(getCategory, 'parentId', null)
-export const getCategorySearchTerm = createGetSelector(getSelectedCategory, 'searchTerm')
+export const getCategorySearchTerm = createGetSelector(getCategory, 'searchTerm')
 export const getCategoryCustomContent = createGetSelector(getSelectedCategory, 'custom', Immutable.Map())
 export const getCategoryDescription = createGetSelector(getSelectedCategory, 'description')
 
