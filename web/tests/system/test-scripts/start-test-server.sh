ba5602f895e03a51acb9af4851377b9d639f9401
jasecode <jzhang@mobify.com>
2017-07-14 10:15:49 -0700

    initialize parallelism and workflows in config.yml

--- web/tests/system/test-scripts/start-test-server.sh
+++ web/tests/system/test-scripts/start-test-server.sh
@@ -6,6 +6,6 @@ set -o nounset
 # Kill background processes when this script exits.
 trap 'kill $(jobs -pr)' EXIT
 echo "Building project"
-cd web && npm run prod:build
+npm run prod:build
 echo "Running Test Server."
 npm run test:server
