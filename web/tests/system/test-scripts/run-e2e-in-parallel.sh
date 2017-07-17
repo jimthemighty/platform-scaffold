96e81cfb84e3ae770a4f29281c09121f993d51fa
jasecode <jzhang@mobify.com>
2017-07-17 15:53:16 -0700

    test using all nodes

--- web/tests/system/test-scripts/run-e2e-in-parallel.sh
+++ web/tests/system/test-scripts/run-e2e-in-parallel.sh
@@ -16,10 +16,9 @@ else
     if [ $CIRCLE_NODE_INDEX -gt 0 ]; then
       ./scripts/wait-for-dependencies.sh
       echo 'Running End to End Tests'
-      #If we have nodes > 2, it will be part of the division to run another test:e2e
       i=0
       for testfile in $(find ./tests/system/workflows/ -name '*.js'| sort); do
-        if [ $(expr $i % $(expr $CIRCLE_NODE_TOTAL - 1)) -eq $(expr $CIRCLE_NODE_INDEX - 1) ]; then
+        if [ $(expr $i % $(expr $CIRCLE_NODE_TOTAL)) -eq $(expr $CIRCLE_NODE_INDEX) ]; then
           echo 'Running test: ' ${testfile}
           npm run test:e2e --test ${testfile}
         fi
