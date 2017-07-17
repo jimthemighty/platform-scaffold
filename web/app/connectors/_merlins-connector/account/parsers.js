ad4e7f0a3bd21075d4f8f04dc258929a3842efa0
Cdok <cdokurno@mobify.com>
2017-07-17 15:51:48 -0700

    moved address parser into function

--- web/app/connectors/_merlins-connector/account/parsers.js
+++ web/app/connectors/_merlins-connector/account/parsers.js
@@ -4,34 +4,37 @@
 
 export const isFormResponseInvalid = ($response, formSelector) => $response.find(formSelector).length
 
+const parseAddressInfo = ($address) => {
+    const text = $address
+        .contents()
+        .filter((_, item) => item.nodeType !== 1)
+        .map((_, textNode) => textNode.textContent)
+
+    const firstname = text[0] ? text[0].split(' ')[0] : ''
+    const lastname = text[0] ? text[0].split(' ')[1] : ''
+    const postcode = text[2].split(',')[2] ? text[2].split(',')[2] : ''
+    const regionId = text[2].split(',')[1] ? text[2].split(',')[1].trim() : ''
+
+    return {
+        firstname,
+        lastname,
+        addressLine1: text[1].trim(),
+        city: text[2].split(',')[0].trim(),
+        countryId: text[3].trim(),
+        postcode,
+        telephone: text[4].trim(),
+        // addressLine2: 'Text',
+        // We expect one of these two "region" fields to be non-null
+        regionId,
+        // region: 'Text'
+    }
+}
 export const accountAddressParser = ($, $html) => {
     return {
-        defaultAddress: $html.find('.block-addresses-default'),
+        defaultAddress: parseAddressInfo($html.find('.block-addresses-default address')),
         addresses: $html.find('address').map((_, address) => {
             const $address = $(address)
-            const text = $address
-                .contents()
-                .filter((_, item) => item.nodeType !== 1)
-                .map((_, textNode) => textNode.textContent)
-
-            const firstname = text[0] ? text[0].split(' ')[0] : ''
-            const lastname = text[0] ? text[0].split(' ')[1] : ''
-            const postcode = text[2].split(',')[2] ? text[2].split(',')[2] : ''
-            const regionId = text[2].split(',')[1] ? text[2].split(',')[1].trim() : ''
-
-            return {
-                firstname,
-                lastname,
-                addressLine1: text[1].trim(),
-                city: text[2].split(',')[0].trim(),
-                countryId: text[3].trim(),
-                postcode,
-                telephone: text[4].trim(),
-                // addressLine2: 'Text',
-                // We expect one of these two "region" fields to be non-null
-                regionId,
-                // region: 'Text'
-            }
+            return parseAddressInfo($address)
         })
     }
 }
