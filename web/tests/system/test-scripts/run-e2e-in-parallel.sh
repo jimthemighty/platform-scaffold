33fbad7f22bc7bee9792eb57d21ab39ba9b379ab
jasecode <jzhang@mobify.com>
2017-07-17 16:04:18 -0700

    use 2 parallelism instead of 3

--- web/tests/system/test-scripts/run-e2e-in-parallel.sh
+++ web/tests/system/test-scripts/run-e2e-in-parallel.sh
@@ -8,22 +8,22 @@ if [ $CIRCLE_NODE_TOTAL -eq 1 ]; then
   npm run test:e2e
 else
   #If the node total is greater than 1 
+  # The other circle_node_index worker will run the rest of the tests
   if [ $CIRCLE_NODE_TOTAL -gt 1 ]; then
     echo $CIRCLE_NODE_TOTAL 'Circle CI nodes. Running tests in parallel.'
     echo 'This is Circle CI node' $CIRCLE_NODE_INDEX'.'
 
-    # The other cirlce_node_index worker will run the rest of the tests
-    if [ $CIRCLE_NODE_INDEX -gt 0 ]; then
-      ./scripts/wait-for-dependencies.sh
-      echo 'Running End to End Tests'
-      i=0
-      for testfile in $(find ./tests/system/workflows/ -name '*.js'| sort); do
-        if [ $(expr $i % $(expr $CIRCLE_NODE_TOTAL)) -eq $(expr $CIRCLE_NODE_INDEX) ]; then
-          echo 'Running test: ' ${testfile}
-          npm run test:e2e --test ${testfile}
-        fi
-        ((i=i+1))
-      done
+    # The other circle_node_index worker will run the rest of the tests
+    ./scripts/wait-for-dependencies.sh
+    echo 'Running End to End Tests'
+    i=0
+    for testfile in $(find ./tests/system/workflows/ -name '*.js'| sort); do
+      if [ $(expr $i % $(expr $CIRCLE_NODE_TOTAL)) -eq $(expr $CIRCLE_NODE_INDEX) ]; then
+        echo 'Running test: ' ${testfile}
+        npm run test:e2e --test ${testfile}
+      fi
+      ((i=i+1))
+    done
     fi
   fi
 fi
