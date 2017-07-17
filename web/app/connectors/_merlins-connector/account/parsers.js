dcb2b3f1465c95575e96518f968381208c108e56
Cdok <cdokurno@mobify.com>
2017-07-17 09:31:48 -0700

    parser for addressList on merlin's connector

--- web/app/connectors/_merlins-connector/account/parsers.js
+++ web/app/connectors/_merlins-connector/account/parsers.js
@@ -6,7 +6,26 @@ export const isFormResponseInvalid = ($response, formSelector) => $response.find
 
 export const accountAddressParser = ($, $html) => {
     return {
-        default: $html.find('.block-addresses-default'),
-        addresses: $html.find('address')
+        defaultAddress: $html.find('.block-addresses-default'),
+        addresses: $html.find('address').map((_, address) => {
+            const $address = $(address)
+            const text = $address
+                .contents()
+                .filter((_, item) => item.nodeType !== 1)
+
+            return {
+                firstname: text[0].textContent.split(' ')[0],
+                lastname: text[0].textContent.split(' ')[1],
+                addressLine1: text[1].textContent.trim(),
+                city: text[2].textContent.split(',')[0].trim(),
+                countryId: text[3].textContent.trim(),
+                postcode: text[2].textContent.split(',')[2].trim(),
+                telephone: text[4].textContent.trim(),
+                // addressLine2: 'Text',
+                // We expect one of these two "region" fields to be non-null
+                regionId: text[2].textContent.split(',')[1].trim(),
+                // region: 'Text'
+            }
+        })
     }
 }
