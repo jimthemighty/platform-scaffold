3b5ac77c2b7106eaaa098f04553226ef3329dde2
Cdok <cdokurno@mobify.com>
2017-07-17 10:00:22 -0700

    parsing of textnodes for merlin's addressbook

--- web/app/connectors/_merlins-connector/account/parsers.js
+++ web/app/connectors/_merlins-connector/account/parsers.js
@@ -12,18 +12,24 @@ export const accountAddressParser = ($, $html) => {
             const text = $address
                 .contents()
                 .filter((_, item) => item.nodeType !== 1)
+                .map((_, textNode) => textNode.textContent)
+
+            const firstname = text[0] ? text[0].split(' ')[0] : ''
+            const lastname = text[0] ? text[0].split(' ')[1] : ''
+            const postcode = text[2].split(',')[2] ? text[2].split(',')[2] : ''
+            const regionId = text[2].split(',')[1] ? text[2].split(',')[1].trim() : ''
 
             return {
-                firstname: text[0].textContent.split(' ')[0],
-                lastname: text[0].textContent.split(' ')[1],
-                addressLine1: text[1].textContent.trim(),
-                city: text[2].textContent.split(',')[0].trim(),
-                countryId: text[3].textContent.trim(),
-                postcode: text[2].textContent.split(',')[2].trim(),
-                telephone: text[4].textContent.trim(),
+                firstname,
+                lastname,
+                addressLine1: text[1].trim(),
+                city: text[2].split(',')[0].trim(),
+                countryId: text[3].trim(),
+                postcode,
+                telephone: text[4].trim(),
                 // addressLine2: 'Text',
                 // We expect one of these two "region" fields to be non-null
-                regionId: text[2].textContent.split(',')[1].trim(),
+                regionId,
                 // region: 'Text'
             }
         })
