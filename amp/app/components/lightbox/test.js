ef05888f15d96a9bc9fbbc216ae474e1ee9db959
Cole Sanderson <cole@mobify.me>
2017-07-18 11:44:50 -0700

    fix lightbox tests

--- amp/app/components/lightbox/test.js
+++ amp/app/components/lightbox/test.js
@@ -2,7 +2,7 @@
 /* Copyright (c) 2017 Mobify Research & Development Inc. All rights reserved. */
 /* * *  *  * *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * */
 
-// import {shallowHOC as shallow} from 'mobify-amp-sdk/dist/test-utils' // Change to '../../test-utils' when move to SDK
+import {shallowHOC as shallow} from 'mobify-amp-sdk/dist/test-utils' // Change to '../../test-utils' when move to SDK
 import {mount} from 'enzyme'
 /* eslint-env jest */
 import React from 'react'
@@ -21,20 +21,15 @@ test('includes the component class name with no className prop', () => {
 })
 
 test('does not render an \'undefined\' class with no className', () => {
-    const wrapper = mount(<Lightbox id="id" />)
+    const wrapper = shallow(<Lightbox id="id" />)
 
     expect(wrapper.hasClass('undefined')).toBe(false)
 })
 
 test('renders the contents of the className prop if present', () => {
-    [
-        'test',
-        'test another'
-    ].forEach((name) => {
-        const wrapper = mount(<Lightbox id="id" className={name} />)
-
-        expect(wrapper.hasClass(name)).toBe(true)
-    })
+    const name = 'test'
+    const wrapper = mount(<Lightbox id="id" className={name} />)
+    expect(wrapper.hasClass(name)).toBe(true)
 })
 
 test('`scrollable` prop set as true, it should add scrollable attribute', () => {
