fafec4dbe8f318ea47ba1d59278a083c2d97c820
Cole Sanderson <cole@mobify.me>
2017-07-18 11:22:31 -0700

    update Lightbox test

--- amp/app/components/lightbox/test.js
+++ amp/app/components/lightbox/test.js
@@ -2,7 +2,7 @@
 /* Copyright (c) 2017 Mobify Research & Development Inc. All rights reserved. */
 /* * *  *  * *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * */
 
-import {shallowHOC as shallow} from '../../test-utils'
+// import {shallowHOC as shallow} from 'mobify-amp-sdk/dist/test-utils' // Change to '../../test-utils' when move to SDK
 import {mount} from 'enzyme'
 /* eslint-env jest */
 import React from 'react'
@@ -11,19 +11,17 @@ import React from 'react'
 import Lightbox from './index.jsx'
 
 test('Lightbox renders without errors', () => {
-    const wrapper = mount(<Lightbox />)
+    const wrapper = mount(<Lightbox id="id" />)
     expect(wrapper.length).toBe(1)
 })
 
-/* eslint-disable newline-per-chained-call */
 test('includes the component class name with no className prop', () => {
-    const wrapper = shallow(<Lightbox />)
-
+    const wrapper = mount(<Lightbox id="id" />)
     expect(wrapper.hasClass('a-lightbox')).toBe(true)
 })
 
 test('does not render an \'undefined\' class with no className', () => {
-    const wrapper = shallow(<Lightbox />)
+    const wrapper = mount(<Lightbox id="id" />)
 
     expect(wrapper.hasClass('undefined')).toBe(false)
 })
@@ -33,8 +31,14 @@ test('renders the contents of the className prop if present', () => {
         'test',
         'test another'
     ].forEach((name) => {
-        const wrapper = shallow(<Lightbox className={name} />)
+        const wrapper = mount(<Lightbox id="id" className={name} />)
 
         expect(wrapper.hasClass(name)).toBe(true)
     })
+})
+
+test('`scrollable` prop set as true, it should add scrollable attribute', () => {
+    const wrapper = mount(<Lightbox id="id" height="10" scrollable={true} />)
+
+    expect(wrapper.find('#id').prop('scrollable')).toBe('')
 })
