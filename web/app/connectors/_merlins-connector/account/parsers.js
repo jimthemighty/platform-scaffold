8de1801c8fb6b5f3adb28a96846fa1b215a0c437
Cdok <cdokurno@mobify.com>
2017-07-18 13:23:01 -0700

    moved merlin's parser address data into variables

--- web/app/connectors/_merlins-connector/account/parsers.js
+++ web/app/connectors/_merlins-connector/account/parsers.js
@@ -10,20 +10,21 @@ const parseAddressInfo = ($address) => {
         .filter((_, item) => item.nodeType !== 1)
         .map((_, textNode) => textNode.textContent)
 
-    const firstname = text[0] ? text[0].split(' ')[0] : ''
-    const lastname = text[0] ? text[0].split(' ')[1] : ''
-    const postcode = text[2].split(',')[2] ? text[2].split(',')[2] : ''
+    const firstname = text[0].split(' ')[0]
+    const lastname = text[0].split(' ')[1]
+    const city = text[2].split(',')[0] ? text[2].split(',')[0].trim() : ''
     const regionId = text[2].split(',')[1] ? text[2].split(',')[1].trim() : ''
+    const postcode = text[2].split(',')[2] ? text[2].split(',')[2].trim() : ''
 
     return {
         firstname,
         lastname,
         addressLine1: text[1].trim(),
-        city: text[2].split(',')[0].trim(),
+        city,
         countryId: text[3].trim(),
+        regionId,
         postcode,
-        telephone: text[4].trim(),
-        regionId
+        telephone: text[4].trim()
     }
 }
 export const accountAddressParser = ($, $html) => {
