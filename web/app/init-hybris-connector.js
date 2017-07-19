14dcbaae31c224e1619c4dace429731804957f4c
Jeremy Wiebe <jeremy@mobify.com>
2017-07-19 12:07:04 -0700

    Add log message that we're registering Hybris connector

--- web/app/init-hybris-connector.js
+++ web/app/init-hybris-connector.js
@@ -2,6 +2,7 @@ import {Connector} from './connectors/hybris-connector'
 import {registerConnector} from 'progressive-web-sdk/dist/integration-manager'
 
 const initConnector = () => {
+    console.log('[IM.hybris-connector] Registering Hybris connector')
     registerConnector(Connector({
         /* Add configuration data here as needed.
          * This can be accessed within the connector
