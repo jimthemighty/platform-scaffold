2cddd470120b80300f7e95b2270dd1e64259ca9b
jasecode <jzhang@mobify.com>
2017-07-20 15:04:47 -0700

    add change-connector.sh

---
+++ web/scripts/change-connector.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Change connector from the first Argument to the second argument
+# eg: ./change-connector.sh merlins sfcc
+sed 's/'$1'/'$2'/; /^\/\/ import initConnector.*$/d' ../app/main.jsx > ../app/main.$2.jsx
+mv -f ../app/main.$2.jsx ../app/main.jsx
