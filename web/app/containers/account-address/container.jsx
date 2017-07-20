f13b2216659f42d97e2f3c4ebde7d3fa3ebca177
gsaray <gsaray@mobify.com>
2017-07-20 15:06:29 -0700

    add no saved address to the container

--- web/app/containers/account-address/container.jsx
+++ web/app/containers/account-address/container.jsx
@@ -9,81 +9,105 @@ import {createPropsSelector} from 'reselect-immutable-helpers'
 import Card from '../../components/card'
 
 import Button from 'progressive-web-sdk/dist/components/button'
+import Image from 'progressive-web-sdk/dist/components/image'
+import {getAssetUrl} from 'progressive-web-sdk/dist/asset-utils'
 import Breadcrumbs from 'progressive-web-sdk/dist/components/breadcrumbs'
 import AddressBlock from './partials/account-address-block'
 import {getDefaultAddress, getAddresses} from '../../store/user/selectors'
 import {UI_NAME} from 'progressive-web-sdk/dist/analytics/data-objects/'
 
+const NoAddress = () => (
+    <div className="t-account-address__empty u-padding-md u-flexbox u-direction-column u-align-center u-justify-center">
+        <Image
+            className="u-flex-none"
+            alt="Crystal Ball"
+            width="122px"
+            height="110px"
+            src={getAssetUrl('static/img/onboarding/location.png')}
+        />
+        <div className="u-text-align-center u-padding-lg">
+            You have no saved addresses.
+        </div>
+        <Button text="Add a new address" href="/" className="pw--tertiary u-width-full u-text-uppercase " />
+    </div>
+)
+
 const AccountAddress = ({defaultAddress, addresses}) => {
     return (
-        <div className="t-account-address">
-            <div className="t-account-address__heading u-padding-top-lg u-padding-bottom-lg u-padding-start-md u-padding-end-md">
-                <div className="t-account-address__breadcrumb">
-                    <Breadcrumbs items={[{text: 'Back to Dashboard', href: '/customer/account'}]} />
-                </div>
-                <div className="u-margin-top-md">
-                    <h1 className="t-account-info__title u-text-uppercase u-width-1of2">Address Book</h1>
-                </div>
-                <Button
-                    text="Add new address"
-                    className="pw--tertiary u-margin-top-lg u-width-full u-text-weight-medium"
-                    data-analytics-name={UI_NAME.addNewAddress}
-                />
-            </div>
-            <div className="t-account-address__content u-padding-md">
-                <Card
-                    hasBorder
-                    header={
-                        <h3 className="u-padding-top-md u-padding-start-md u-padding-end-md">Default address</h3>
-                    }
-                    children={<AddressBlock {...defaultAddress} />}
-                    footer={
+        <div>
+            {defaultAddress ?
+                <div className="t-account-address">
+                    <div className="t-account-address__heading u-padding-top-lg u-padding-bottom-lg u-padding-start-md u-padding-end-md">
+                        <div className="t-account-address__breadcrumb">
+                            <Breadcrumbs items={[{text: 'Back to Dashboard', href: '/customer/account'}]} />
+                        </div>
+                        <div className="u-margin-top-md">
+                            <h1 className="t-account-info__title u-text-uppercase u-width-1of2">Address Book</h1>
+                        </div>
                         <Button
-                            type="button"
-                            title="Change Address"
-                            className="u-width-full u-color-brand u-border-top"
-                            icon="edit"
-                            showIconText={true}
-                            iconClassName="u-margin-end"
-                            data-analytics-name={UI_NAME.editSavedAddress}
+                            text="Add new address"
+                            className="pw--tertiary u-margin-top-lg u-width-full u-text-weight-medium"
+                            data-analytics-name={UI_NAME.addNewAddress}
                         />
-                    }
-                />
-                {addresses.map((address, idx) => {
-                    return (
-                        <Card key={idx}
+                    </div>
+                    <div className="t-account-address__content u-padding-md">
+                        <Card
                             hasBorder
-                            children={<AddressBlock {...address} />}
+                            header={
+                                <h3 className="u-padding-top-md u-padding-start-md u-padding-end-md">Default address</h3>
+                            }
+                            children={<AddressBlock {...defaultAddress} />}
                             footer={
-                                <div className="u-flexbox">
-                                    <div className="u-flex u-border-end">
-                                        <Button
-                                            type="button"
-                                            title="Edit"
-                                            className="u-width-full u-color-brand u-border-top"
-                                            icon="edit"
-                                            showIconText={true}
-                                            iconClassName="u-margin-end"
-                                            data-analytics-name={UI_NAME.editSavedAddress}
-                                        />
-                                    </div>
-                                    <div className="u-flex">
-                                        <Button
-                                            type="button"
-                                            title="Delete"
-                                            className="u-width-full u-color-brand u-border-top"
-                                            icon="trash"
-                                            showIconText={true}
-                                            iconClassName="u-margin-end"
-                                            data-analytics-name={UI_NAME.removeSavedAddress}
-                                        />
-                                    </div>
-                                </div>
+                                <Button
+                                    type="button"
+                                    title="Change Address"
+                                    className="u-width-full u-color-brand u-border-top"
+                                    icon="edit"
+                                    showIconText={true}
+                                    iconClassName="u-margin-end"
+                                    data-analytics-name={UI_NAME.editSavedAddress}
+                                />
                             }
                         />
-                    )
-                })}
-            </div>
+                        {addresses.map((address, idx) => {
+                            return (
+                                <Card key={idx}
+                                    hasBorder
+                                    children={<AddressBlock {...address} />}
+                                    footer={
+                                        <div className="u-flexbox">
+                                            <div className="u-flex u-border-end">
+                                                <Button
+                                                    type="button"
+                                                    title="Edit"
+                                                    className="u-width-full u-color-brand u-border-top"
+                                                    icon="edit"
+                                                    showIconText={true}
+                                                    iconClassName="u-margin-end"
+                                                    data-analytics-name={UI_NAME.editSavedAddress}
+                                                />
+                                            </div>
+                                            <div className="u-flex">
+                                                <Button
+                                                    type="button"
+                                                    title="Delete"
+                                                    className="u-width-full u-color-brand u-border-top"
+                                                    icon="trash"
+                                                    showIconText={true}
+                                                    iconClassName="u-margin-end"
+                                                    data-analytics-name={UI_NAME.removeSavedAddress}
+                                                />
+                                            </div>
+                                        </div>
+                                    }
+                                />
+                            )
+                        })}
+                    </div>
+                </div>
+            :
+                <NoAddress />
+            }
         </div>
     )
 }
