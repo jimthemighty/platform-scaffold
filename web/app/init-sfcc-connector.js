392403d600bf372209860cd78f2ef1ae6408c26e
Jeremy Wiebe <jeremy@mobify.com>
2017-07-24 16:18:40 -0700

    Remove nav types from SFCC config. No longer needed.

--- web/app/init-sfcc-connector.js
+++ web/app/init-sfcc-connector.js
@@ -1,13 +1,10 @@
 import {Connector} from 'progressive-web-sdk/dist/connectors/sfcc-connector'
 import {registerConnector} from 'progressive-web-sdk/dist/integration-manager'
-import {SIGNED_IN_NAV_ITEM_TYPE, GUEST_NAV_ITEM_TYPE} from './modals/navigation/constants'
 
 const initConnector = () => {
     registerConnector(Connector({
         siteID: '2017refresh',
-        clientID: '5640cc6b-f5e9-466e-9134-9853e9f9db93',
-        signedInNavType: SIGNED_IN_NAV_ITEM_TYPE,
-        guestNavType: GUEST_NAV_ITEM_TYPE
+        clientID: '5640cc6b-f5e9-466e-9134-9853e9f9db93'
     }))
 }
 
