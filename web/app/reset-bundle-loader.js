73e59416334494380546de2e0f6b30bc4897134c
Ben Last <ben@benlast.com>
2017-07-25 14:45:12 -0700

    Add minimal reset-bundle-loader.js

---
+++ web/app/reset-bundle-loader.js
@@ -0,0 +1,7 @@
+/* * *  *  * *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * */
+/* Copyright (c) 2017 Mobify Research & Development Inc. All rights reserved. */
+/* * *  *  * *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * */
+import {shouldPreview, loadPreview} from 'progressive-web-sdk/dist/utils/preview-utils'
+if (shouldPreview()) {
+    loadPreview()
+}
