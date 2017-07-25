615234b157893f203e8bb54584ed30d5b4bde4be
Jennifer Bandelin <jennifer@mobify.com>
2017-07-25 14:52:03 -0700

    Add wrapper container action for addToCartFromWishlist command

--- web/app/containers/wishlist/actions.js
+++ web/app/containers/wishlist/actions.js
@@ -1,4 +1,10 @@
 /* * *  *  * *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * */
 /* Copyright (c) 2017 Mobify Research & Development Inc. All rights reserved. */
 /* * *  *  * *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * */
+import {
+    addToCartFromWishlist as addToCartFromWishlistCommand
+} from 'progressive-web-sdk/dist/integration-manager/account/commands'
 
+export const addToCartFromWishlist = (itemID, quantity) => (dispatch) => {
+    return dispatch(addToCartFromWishlistCommand({itemID, quantity}))
+}
