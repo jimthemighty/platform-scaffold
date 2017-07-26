cd24bfde48768436a02ea4004215dab28005ab66
Ben Last <ben@benlast.com>
2017-07-26 13:10:57 -0700

    PR feedback

--- web/app/reset-bundle-loader.js
+++ web/app/reset-bundle-loader.js
@@ -2,6 +2,9 @@
 /* Copyright (c) 2017 Mobify Research & Development Inc. All rights reserved. */
 /* * *  *  * *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * */
 import {shouldPreview, loadPreview} from 'progressive-web-sdk/dist/utils/preview-utils'
+// shouldPreview() returns true if the appropriate cookie is set to
+// enable preview, and we're not already running in preview.
 if (shouldPreview()) {
+    // If preview is required, this will load the preview script.
     loadPreview()
 }
