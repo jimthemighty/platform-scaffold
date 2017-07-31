75602a30631038adef2712b252b276e1a541ffa5
fractaltheory <michael@mobify.com>
2017-07-31 14:55:51 -0700

    Here we gooooo

--- web/scripts/setup-env-ci.sh
+++ web/scripts/setup-env-ci.sh
@@ -6,5 +6,5 @@ printf "\nInstall Chrome: "
 nohup bash -c "./scripts/update-chrome-accept-cert-ci.sh &> logs/apt-get.log &"
 printf "\nNPM Install: "
 npm install
-printf "\nStart Test Server: "
-nohup bash -c "./tests/e2e/test-scripts/start-test-server.sh &> logs/startTestServer.log &"
+# printf "\nStart Test Server: "
+#nohup bash -c "./tests/e2e/test-scripts/start-test-server.sh &> logs/startTestServer.log &"
