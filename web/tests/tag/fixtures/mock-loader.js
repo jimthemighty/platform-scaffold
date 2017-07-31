1a5d5186db4ea385ec89246e0e32437d0e44e148
Ben Last <ben@benlast.com>
2017-07-31 13:24:27 -0700

    Add missing copyright header

--- web/tests/tag/fixtures/mock-loader.js
+++ web/tests/tag/fixtures/mock-loader.js
@@ -1,1 +1,4 @@
+/* * *  *  * *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * */
+/* Copyright (c) 2017 Mobify Research & Development Inc. All rights reserved. */
+/* * *  *  * *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * */
 console.log('Mock loader has loaded')
