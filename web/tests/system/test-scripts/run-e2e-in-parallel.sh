991796b288f83e5abb660be558170cea1dd4d236
jasecode <jzhang@mobify.com>
2017-08-01 18:07:31 -0700

    experimenting with e2e

--- web/tests/system/test-scripts/run-e2e-in-parallel.sh
+++ web/tests/system/test-scripts/run-e2e-in-parallel.sh
@@ -2,23 +2,34 @@
 set -o pipefail
 set -o nounset
 
-#If the node total is 1, run all the tests sequentially. 
-if [ $CIRCLE_NODE_TOTAL -eq 1 ]; then
-  echo 'Running End to End Tests'
-  npm run test:e2e
-else
-  #If the node total is greater than 1 
-  # The other circle_node_index worker will run the rest of the tests
-  if [ $CIRCLE_NODE_TOTAL -gt 1 ]; then
-    echo $CIRCLE_NODE_TOTAL 'Circle CI nodes. Running tests in parallel.'
-    echo 'This is Circle CI node' $CIRCLE_NODE_INDEX'.'
+#If the node total is greater than 1 
+if [ $CIRCLE_NODE_TOTAL -gt 1 ]; then
+  echo $CIRCLE_NODE_TOTAL 'Circle CI nodes. Running tests in parallel.'
+  echo 'This is Circle CI node' $CIRCLE_NODE_INDEX'.'
 
-    # The other circle_node_index worker will run the rest of the tests
+  #Assign the first node to running lighthouse Tests
+  if [ $CIRCLE_NODE_INDEX -eq 0 ]; then
+    echo 'Running Lint'
+    npm run lint     
     ./scripts/wait-for-dependencies.sh
+    echo 'Running Lighthouse Test'
+    ./tests/system/test-scripts/run-lighthouse.sh
+  fi
+
+  # The other cirlce_node_index worker will run the rest of the tests
+  if [ $CIRCLE_NODE_INDEX -gt 0 ]; then
+    echo 'Running Unit Tests'
+    npm test -- --runInBand
+
+    ./scripts/wait-for-dependencies.sh
+    echo 'Verify built files sizes'
+    npm run test:build-size
+  
     echo 'Running End to End Tests'
+    #If we have nodes > 2, it will be part of the division to run another test:e2e
     i=0
     for testfile in $(find ./tests/system/workflows/ -name '*.js'| sort); do
-      if [ $(expr $i % $(expr $CIRCLE_NODE_TOTAL)) -eq $(expr $CIRCLE_NODE_INDEX) ]; then
+      if [ $(expr $i % $(expr $CIRCLE_NODE_TOTAL - 1)) -eq $(expr $CIRCLE_NODE_INDEX - 1) ]; then
         echo 'Running test: ' ${testfile}
         npm run test:e2e --test ${testfile}
       fi
