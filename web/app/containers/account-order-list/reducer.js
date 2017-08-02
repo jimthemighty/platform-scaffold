7e0c41c7379b5fbb028609dea790383fd90634de
Cdok <cdokurno@mobify.com>
2017-08-02 10:24:30 -0700

    removed commented out reducer contents

--- web/app/containers/account-order-list/reducer.js
+++ web/app/containers/account-order-list/reducer.js
@@ -1,13 +1,3 @@
 /* * *  *  * *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * */
 /* Copyright (c) 2017 Mobify Research & Development Inc. All rights reserved. */
 /* * *  *  * *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * */
-
-// import {handleActions} from 'redux-actions'
-// import Immutable from 'immutable'
-// import {mergePayload} from 'progressive-web-sdk/dist/utils/reducer-utils'
-
-
-// const initialState = Immutable.Map()
-
-// export default handleActions({
-// }, initialState)
