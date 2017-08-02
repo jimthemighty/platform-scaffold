9098b3eeb790501b853f8866bb1bc387c33b8da8
jasecode <jzhang@mobify.com>
2017-08-02 11:45:48 -0700

    renaming things

--- web/tests/e2e/test-scripts/run-e2e-in-parallel.sh
+++ web/tests/e2e/test-scripts/run-e2e-in-parallel.sh
@@ -5,7 +5,7 @@ set -o nounset
 echo 'Running End to End Tests'
 #If we have nodes > 2, it will be part of the division to run another test:e2e
 i=0
-for testfile in $(find ./tests/system/workflows/ -name '*.js'| sort); do
+for testfile in $(find ./tests/e2e/workflows/ -name '*.js'| sort); do
   if [ $(expr $i % $(expr $CIRCLE_NODE_TOTAL - 1)) -eq $(expr $CIRCLE_NODE_INDEX - 1) ]; then
     echo 'Running test: ' ${testfile}
     npm run test:e2e --test ${testfile}
