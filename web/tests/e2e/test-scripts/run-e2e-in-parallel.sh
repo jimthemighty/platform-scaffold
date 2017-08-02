67a53e9d16c37370032ee261360329564908e323
jasecode <jzhang@mobify.com>
2017-08-02 11:55:05 -0700

    fix parallelism

--- web/tests/e2e/test-scripts/run-e2e-in-parallel.sh
+++ web/tests/e2e/test-scripts/run-e2e-in-parallel.sh
@@ -6,9 +6,11 @@ echo 'Running End to End Tests'
 #If we have nodes > 2, it will be part of the division to run another test:e2e
 i=0
 for testfile in $(find ./tests/e2e/workflows/ -name '*.js'| sort); do
-  if [ $(expr $i % $(expr $CIRCLE_NODE_TOTAL - 1)) -eq $(expr $CIRCLE_NODE_INDEX - 1) ]; then
+  if [ $(expr $i % $CIRCLE_NODE_TOTAL) -eq $CIRCLE_NODE_INDEX ]; then
     echo 'Running test: ' ${testfile}
     npm run test:e2e --test ${testfile}
   fi
   ((i=i+1))
 done
+
+
