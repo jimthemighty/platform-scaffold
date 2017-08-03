6f7253fd37f73b1ad0371418f95238b1d1c6e7f6
gsaray <gsaray@mobify.com>
2017-08-03 10:09:47 -0700

    remove lighthouse and e2e tests for now

--- web/tests/e2e/test-scripts/run-all-tests-on-ci.sh
+++ web/tests/e2e/test-scripts/run-all-tests-on-ci.sh
@@ -2,7 +2,7 @@
 set -o pipefail
 set -o nounset
 
-#If the node total is 1, run all the tests sequentially. 
+#If the node total is 1, run all the tests sequentially.
 if [ $CIRCLE_NODE_TOTAL -eq 1 ]; then
   echo 'Running lint'
   npm run lint
@@ -11,13 +11,13 @@ if [ $CIRCLE_NODE_TOTAL -eq 1 ]; then
   ./scripts/wait-for-dependencies.sh
   echo 'Verify built files sizes'
   npm run test:max-file-size -- build tests/performance/gzip-size-config.json
-  echo 'Starting Lighthouse Tests.'
-  ./tests/performance/lighthouse/run-lighthouse.sh
-  echo 'Running End to End Tests'
-  npm run test:e2e
+  # echo 'Starting Lighthouse Tests.'
+  # ./tests/performance/lighthouse/run-lighthouse.sh
+  # echo 'Running End to End Tests'
+  # npm run test:e2e
 
 else
-  #If the node total is greater than 1 
+  #If the node total is greater than 1
   if [ $CIRCLE_NODE_TOTAL -gt 1 ]; then
     echo $CIRCLE_NODE_TOTAL 'Circle CI nodes. Running tests in parallel.'
     echo 'This is Circle CI node' $CIRCLE_NODE_INDEX'.'
@@ -25,10 +25,10 @@ else
     #Assign the first node to running lighthouse Tests
     if [ $CIRCLE_NODE_INDEX -eq 0 ]; then
       echo 'Running Lint'
-      npm run lint     
+      npm run lint
       ./scripts/wait-for-dependencies.sh
-      echo 'Running Lighthouse Test'
-      ./tests/performance/lighthouse/run-lighthouse.sh
+    #   echo 'Running Lighthouse Test'
+    #   ./tests/performance/lighthouse/run-lighthouse.sh
     fi
 
     # The other cirlce_node_index worker will run the rest of the tests
@@ -39,17 +39,17 @@ else
       ./scripts/wait-for-dependencies.sh
       echo 'Verify built files sizes'
       npm run test:max-file-size -- build tests/performance/gzip-size-config.json
-    
-      echo 'Running End to End Tests'
+
+    #   echo 'Running End to End Tests'
       #If we have nodes > 2, it will be part of the division to run another test:e2e
-      i=0
-      for testfile in $(find ./tests/e2e/workflows/ -name '*.js'| sort); do
-        if [ $(expr $i % $(expr $CIRCLE_NODE_TOTAL - 1)) -eq $(expr $CIRCLE_NODE_INDEX - 1) ]; then
-          echo 'Running test: ' ${testfile}
-          npm run test:e2e --test ${testfile}
-        fi
-        ((i=i+1))
-      done
+    #   i=0
+    #   for testfile in $(find ./tests/e2e/workflows/ -name '*.js'| sort); do
+    #     if [ $(expr $i % $(expr $CIRCLE_NODE_TOTAL - 1)) -eq $(expr $CIRCLE_NODE_INDEX - 1) ]; then
+    #       echo 'Running test: ' ${testfile}
+    #       npm run test:e2e --test ${testfile}
+    #     fi
+    #     ((i=i+1))
+    #   done
     fi
   fi
 fi
