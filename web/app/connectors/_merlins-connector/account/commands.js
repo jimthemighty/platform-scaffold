742d895ba2e2e64a0499330e2d8a5cac46cd189b
Cdok <cdokurno@mobify.com>
2017-08-17 16:17:04 -0700

    removed trailing whitespace

--- web/app/connectors/_merlins-connector/account/commands.js
+++ web/app/connectors/_merlins-connector/account/commands.js
@@ -347,7 +347,7 @@ export const reorderPreviousOrder = (orderNumber) => (dispatch, getState) => { /
     const formKey = getFormKey(getState())
     const orderId = orderNumber.replace(/^0+/, '')
     return makeFormEncodedRequest(`/sales/order/reorder/order_id/${orderId}/`, {form_key: formKey}, {method: 'POST'})
-        .then(() => CART_URL) 
+        .then(() => CART_URL)
 }
 
 export const updateWishlistItem = (itemId, wishlistId, quantity) => (dispatch, getState) => {
