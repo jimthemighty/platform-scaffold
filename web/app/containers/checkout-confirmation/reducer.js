448c4a5b58dd947e172458250df9e93aaba5381c
Jeremy Wiebe <jeremy@mobify.me>
2017-08-20 15:12:58 -0700

    Don't reduce thunks created for analytics

--- web/app/containers/checkout-confirmation/reducer.js
+++ web/app/containers/checkout-confirmation/reducer.js
@@ -5,13 +5,11 @@
 import {handleActions} from 'redux-actions'
 import Immutable from 'immutable'
 import * as checkoutConfirmationActions from './actions'
-import {realReceiveCheckoutConfirmationData} from 'progressive-web-sdk/dist/integration-manager/checkout/results'
 import {mergePayload} from 'progressive-web-sdk/dist/utils/reducer-utils'
 import {receiveOrderConfirmationContents} from 'progressive-web-sdk/dist/integration-manager/results'
 
 export default handleActions({
     [receiveOrderConfirmationContents]: mergePayload,
-    [realReceiveCheckoutConfirmationData]: mergePayload,
     [checkoutConfirmationActions.hideRegistrationForm]: (state) => {
         return state.set('isRegistrationFormHidden', true)
     }
