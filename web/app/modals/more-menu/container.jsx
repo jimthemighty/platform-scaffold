8dcb9d687d7798ddef0ae58225f783caefea09ed
Jennifer Bandelin <jennifer@mobify.com>
2017-08-21 17:34:05 -0700

    Style and animate more menu

--- web/app/modals/more-menu/container.jsx
+++ web/app/modals/more-menu/container.jsx
@@ -4,35 +4,102 @@
 
 import React from 'react'
 import {connect} from 'react-redux'
+import classNames from 'classnames'
 import {createPropsSelector} from 'reselect-immutable-helpers'
 import {MORE_MENU} from '../constants'
 import {closeModal} from 'progressive-web-sdk/dist/store/modals/actions'
 import {isModalOpen} from 'progressive-web-sdk/dist/store/modals/selectors'
 import {getRemoveItemID} from '../../containers/cart/selectors'
 
-import Sheet from 'progressive-web-sdk/dist/components/sheet'
+import Button from 'progressive-web-sdk/dist/components/button'
 import {UI_NAME} from 'progressive-web-sdk/dist/analytics/data-objects/'
 
-const MoreMenuModal = ({closeModal, isOpen, duration}) => {
-    return (
-        <Sheet
-            className="pw--no-shadow m-cart__remove-item-confirmation-modal"
-            open={isOpen}
-            onDismiss={closeModal}
-            duration={duration}
-            maskOpacity={0}
-            effect="modal-center"
-            shrinkToContent={true}
-            coverage="40%"
-        >
-            <div className="u-flexbox u-direction-column u-align-center u-padding-md u-padding-top-lg u-padding-bottom-lg u-text-align-center">
-                This is some menu content
+import {getAccountURL} from '../../containers/app/selectors'
+
+import {LOCATION_URL} from '../../containers/app/constants'
+
+
+class MoreMenuModal extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {active: false}
+    }
+
+    componentWillMount() {
+        setTimeout(() => {
+            requestAnimationFrame(() => this.setState({active: true}))
+        }, 100)
+    }
+
+    render() {
+        const {closeModal, accountURL, isOpen} = this.props
+        const modalClasses = classNames('m-more-menu', {
+            'm--active': isOpen && this.state.active
+        })
+
+        return (
+            <div>
+                <div className={modalClasses}>
+                    <div className="m-more-menu__content u-align-center u-padding-md u-padding-bottom-lg">
+                        <div>
+                            <Button
+                                className="u-width-block-full"
+                                innerClassName="u-color-neutral-60 u-justify-start"
+                                iconClassName="u-color-brand"
+                                href={LOCATION_URL}
+                                icon="map"
+                                data-analytics-name={UI_NAME.showStoreLocator}
+                                title="Store Locations"
+                                showIconText
+                            />
+                            <Button
+                                innerClassName="u-color-neutral-60 u-justify-start"
+                                iconClassName="u-color-brand"
+                                href={accountURL}
+                                icon="user"
+                                data-analytics-name={UI_NAME.showStoreLocator}
+                                title="My Account"
+                                showIconText
+                            />
+                        </div>
+                        <div className="u-border-top u-width-full">
+                            <Button
+                                className="u-width-block-full"
+                                innerClassName="u-justify-start"
+                                text="Forward"
+                                onClick={() => { window.history.forward() }}
+                            />
+
+                            <Button
+                                className="u-width-block-full"
+                                innerClassName="u-justify-start"
+                                text="Refresh"
+                                onClick={() => { window.location.reload() }}
+                            />
+
+                            <Button
+                                className="u-width-block-full"
+                                innerClassName="u-justify-start"
+                                text="Share..."
+                                onClick={() => { alert('SHARE THIS URL!!') }}
+                            />
+                        </div>
+                    </div>
+                </div>
+
+                <div
+                    tabIndex="-1"
+                    role="presentation"
+                    className="m-more-menu__shade"
+                    onTouchStart={closeModal}
+                />
             </div>
-        </Sheet>
-    )
+        )
+    }
 }
 
 MoreMenuModal.propTypes = {
+    accountURL: React.PropTypes.string,
     /**
      * A function used to set the navigation-sheet's state to closed
      */
@@ -56,6 +123,7 @@ MoreMenuModal.propTypes = {
 }
 
 const mapStateToProps = createPropsSelector({
+    accountURL: getAccountURL,
     isOpen: isModalOpen(MORE_MENU),
     removeItemID: getRemoveItemID
 })
