5d7a94c160bb5107f67dd5cf33b10288a3c5df70
Cdok <cdokurno@mobify.com>
2017-08-24 11:21:43 -0700

    fixed showBackButton selector

--- web/app/containers/header/selectors.js
+++ web/app/containers/header/selectors.js
@@ -2,18 +2,19 @@
 /* Copyright (c) 2017 Mobify Research & Development Inc. All rights reserved. */
 /* * *  *  * *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * */
 
+import Immutable from 'immutable'
 import {createSelector} from 'reselect'
 import {createGetSelector} from 'reselect-immutable-helpers'
 import {getUi} from '../../store/selectors'
 import {isStandaloneApp} from '../app/selectors'
 
 export const getHeader = createSelector(getUi, ({header}) => header)
-export const getBrowsingHistory = createGetSelector(getHeader, 'appHistory')
+export const getBrowsingHistory = createGetSelector(getHeader, 'appHistory', Immutable.List())
 export const getIsCollapsed = createGetSelector(getHeader, 'isCollapsed')
 export const getSearchIsOpen = createGetSelector(getHeader, 'searchIsOpen')
 export const getSearchSuggestions = createGetSelector(getHeader, 'searchSuggestions')
 export const showBackButton = createSelector(
     getBrowsingHistory,
     isStandaloneApp,
-    (browsingHistory, isStandaloneApp) => isStandaloneApp && browsingHistory && browsingHistory.length > 1)
+    (browsingHistory, isStandaloneApp) => isStandaloneApp && browsingHistory.size > 1)
 
