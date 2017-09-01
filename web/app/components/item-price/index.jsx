f828755a32a45ea13df888e0a1849a450b44d2f1
Kevin He <khe@mobify.me>
2017-09-01 16:26:40 -0700

    cross out original price

--- web/app/components/item-price/index.jsx
+++ web/app/components/item-price/index.jsx
@@ -14,13 +14,27 @@ import Field from 'progressive-web-sdk/dist/components/field'
 const ItemPrice = ({
     linePrice,
     itemPrice,
-    className
+    className,
+    quantity,
+    originalPrice
 }) => {
     const wrapperClass = classnames(className, 'c-item-price')
+
+    const linePriceValue = parseFloat(linePrice.substring(1))
+    const priceValue = parseFloat(originalPrice.substring(1)) * quantity
+    const discount = linePriceValue < priceValue
+
     return (
         <Field className={wrapperClass}>
             <div className="u-text-align-end u-flex">
-                <div className="u-h5 u-color-accent u-text-weight-bold">{linePrice}</div>
+                {discount ?
+                    <div>
+                        <span className="u-h5 u-color-accent u-text-weight-bold">{linePrice}</span>
+                        <span className="u-text-quiet u-text-strikethrough u-padding-start">{`$${priceValue}`}</span>
+                    </div>
+                :
+                    <div className="u-h5 u-color-accent u-text-weight-bold">{linePrice}</div>
+                }
                 {itemPrice &&
                     <div className="u-text-quiet"><em>{itemPrice} each</em></div>
                 }
@@ -39,7 +53,15 @@ ItemPrice.propTypes = {
     /**
      * The full price for the line item (unit price * quantity)
     */
-    linePrice: PropTypes.string
+    linePrice: PropTypes.string,
+    /**
+     * The original unit price for the item
+    */
+    originalPrice: PropTypes.string,
+    /**
+     * The quantity of this item
+    */
+    quantity: PropTypes.number,
 }
 
 export default ItemPrice
